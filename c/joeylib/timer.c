7cbc0a53a9161f76a40590eaa1a2c3ebc0d49815
joey <joey>
2001-08-29 15:44:14 +0000

    #defines clock to uclock for DOS, because it is much finer resolution

--- c/joeylib/timer.c
+++ c/joeylib/timer.c
@@ -6,34 +6,24 @@ int numframes;
 long endtime;
 long pausetime;
 
+#ifdef DOS
+	#define clock uclock
+	#define CLOCKS_PER_SEC UCLOCKS_PER_SEC
+#endif
+
 void pausetimer() {
   printf("Pausing timer.\n");
-  #ifdef DOS
-  pausetime=uclock();
-  #endif
-  #ifdef POSIX
     pausetime=clock();
-  #endif
 }
 
 void unpausetimer() {
   printf("Un-pausing timer.\n");
   long toadd=pausetime-starttime;
-  #ifdef DOS
-  starttime=uclock()-toadd;
-  #endif
-  #ifdef POSIX
     starttime=clock()-toadd;
-  #endif
 }
 
 void starttimer() {
-  #ifdef DOS
-  starttime=uclock();
-  #endif
-  #ifdef POSIX
     starttime=clock();
-  #endif
   numframes=0;
   endtime=-1;
 }
@@ -43,39 +33,25 @@ void framedone() {
 }
 
 void savetimer() {
-  #ifdef DOS
-  endtime=uclock();
-  #endif
-  #ifdef POSIX
     endtime=clock();
-  #endif
 }
 
 void displayframespersecond() {
   if (endtime==-1)
     savetimer();
-  #ifdef DOS
-  printf("%f frames per second.\n",(float)UCLOCKS_PER_SEC*numframes/(endtime-starttime));
-  #endif
-  #ifdef POSIX
     printf("%f frames per second.\n",(float)numframes/(float)(endtime-starttime)*(float)CLOCKS_PER_SEC);
-  #endif
 }
 
-void waituntil(float seconds) {
+void waituntil(float seconds) { // Wot exacly is seconds?  Is starttime expected set?!
   #ifdef DOS
   while (uclock()-starttime<UCLOCKS_PER_SEC*seconds) {
     // Do nothing
+		// Should release processor
   }
   #endif
 }
 
 float gettimer() {
   savetimer();
-  #ifdef DOS
-    return (float)(endtime-starttime)/(float)UCLOCKS_PER_SEC;
-  #endif
-  #ifdef POSIX
     return (float)(endtime-starttime)/(float)CLOCKS_PER_SEC;
-  #endif
 }
