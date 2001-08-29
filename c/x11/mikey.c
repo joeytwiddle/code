bc4d27cc092b03142294091de3a107d293fa36dd
joey <joey>
2001-08-29 04:35:36 +0000

    Compiles for X now - 7 fps in 256colour mode. I assume compiles for DOS too =)

--- c/x11/mikey.c
+++ c/x11/mikey.c
@@ -33,29 +33,23 @@
   #include <unistd.h>
   #include <sched.h>
 
-/* Some globals */
-int can_use_mitshm = 1;
-int mitshm_major_code;
-int mitshm_minor_code;
-
-/* error handler to catch MITSHM problems */
-int mitshm_handler (Display * d, XErrorEvent * ev)
-{
-  can_use_mitshm = 0;
-  return 0;
-}
-
+  /* Some globals */
+  int can_use_mitshm = 1;
+  int mitshm_major_code;
+  int mitshm_minor_code;
+  
   Display *d;
   XImage *ximg = NULL;
   GC gc;
 
   Window win;
   XColor xrgb[COLORS];
+  XEvent ev;
 
-Colormap colormap;
+  Colormap colormap;
 	static int styleeTrueColor = 0 ;
 	static int styleePrivate   = 1 ;
-    unsigned long color[COLORS];
+  unsigned long color[COLORS];
 
 
 #endif
@@ -71,7 +65,7 @@ JBmp img,img2,imgtmp;
 // Memory leak is here
 void wicked_blit() {
 
-  #ifdef X!!
+  #ifdef X11GFX
       if (can_use_mitshm)
 				XShmPutImage (d, win, gc, ximg, 0, 0, 0, 0, WIN_W, WIN_H, True);
       else
@@ -80,7 +74,7 @@ void wicked_blit() {
 	    XSync (d, 0); // this stopped it going mad
 		  // wait(0.01);
 		sched_yield();
-			printf(".\n");
+			// printf(".\n");
   #endif
   #ifdef ALLEGRO
     img2.blit();
@@ -329,7 +323,7 @@ void iterate_candy() {
 
         #ifdef X11GFX
           // Fake palette switching
-          c=(c+palspeed*frame)%colors;
+          c=(c+palspeed*frameno)%colors;
           ximg->f.put_pixel(ximg,x,y,xrgb[c].pixel);
         #endif
 				
@@ -620,7 +614,13 @@ void real_main() {
 
 #ifdef X11GFX
 
-  XEvent ev;
+  /* error handler to catch MITSHM problems */
+  int mitshm_handler (Display * d, XErrorEvent * ev)
+  {
+    can_use_mitshm = 0;
+    return 0;
+  }
+
   XShmSegmentInfo shminfo;
   int screen;
   XVisualInfo vis, *vlist;
@@ -628,193 +628,196 @@ void real_main() {
   struct rusage resource_utilization;
   int shared_pixmaps;		/* dummy */
 
-int get_xvisinfo_class(XVisualInfo x) {
-					return 1;
-				}
-	
-int main (void)
-{
-  int i;
+  int get_xvisinfo_class(XVisualInfo x) {
+					  return 1;
+				  }
+  	
+  int main (void)
+  {
+    int i;
+  
+    int allocateOK;
+  
+    ximg = NULL;
+  
+  	
+    d = XOpenDisplay (NULL);
+  
+    if (!d)
+      fputs ("Couldn't open display\n", stderr), exit (1);
+  
+    screen = DefaultScreen (d);
+    gc = DefaultGC (d, screen);
+  
+    /* Find a visual */
+  
+    vis.screen = screen;
+    vlist = XGetVisualInfo (d, VisualScreenMask, &vis, &match);
+  
+    if (!vlist)
+      fputs ("No matched visuals\n", stderr), exit (1);
+  
+    vis = vlist[0];
+    XFree (vlist);
+  
+    if (vis.colormap_size < COLORS)
+      fputs ("Colormap is too small.\n", stderr); // , exit (1);
+  
+  
+    win = XCreateSimpleWindow (d, DefaultRootWindow (d),
+			       0, 0, WIN_W, WIN_H, 0,
+			       WhitePixel (d, screen), BlackPixel (d, screen));
+  
+	  int xclass=get_xvisinfo_class(vis);
+	  stylee = ( vis.depth > 8 ? styleeTrueColor : styleePrivate );
+	  printf("stylee=%i\n",stylee);
+  
+    if ( get_xvisinfo_class(vis) % 2 == 1) {	/* The odd numbers can redefine colors */
+  
+			  // printf("%i\n",get_xvisinfo_class(vis));
+	  	
+      colormap = DefaultColormap (d, screen);
+		  Visual *defaultVisual=DefaultVisual(d,screen);
+	  	
+      /* Allocate cells */
+      allocateOK = (XAllocColorCells (d, colormap, 1,
+							      NULL, 0, color, COLORS) != 0);
+      if (allocateOK) {
+  
+			  printf("Allocated OK\n");
+        // This doesn't work for installed colormap!
+  
+        /* Modify the colorcells */
+        for (i = 0; i < COLORS; i++)
+				  xrgb[i].pixel = color[i];
+  
+        XStoreColors (d, colormap, xrgb, COLORS);
+  
+		  } else {
 
-  int allocateOK;
-
-  ximg = NULL;
-
-	
-  d = XOpenDisplay (NULL);
-
-  if (!d)
-    fputs ("Couldn't open display\n", stderr), exit (1);
-
-  screen = DefaultScreen (d);
-  gc = DefaultGC (d, screen);
-
-  /* Find a visual */
-
-  vis.screen = screen;
-  vlist = XGetVisualInfo (d, VisualScreenMask, &vis, &match);
-
-  if (!vlist)
-    fputs ("No matched visuals\n", stderr), exit (1);
-
-  vis = vlist[0];
-  XFree (vlist);
-
-  if (vis.colormap_size < COLORS)
-    fputs ("Colormap is too small.\n", stderr); // , exit (1);
-
-
-  win = XCreateSimpleWindow (d, DefaultRootWindow (d),
-			     0, 0, WIN_W, WIN_H, 0,
-			     WhitePixel (d, screen), BlackPixel (d, screen));
-
-	int xclass=get_xvisinfo_class(vis);
-	stylee = ( vis.depth > 8 ? styleeTrueColor : styleePrivate );
-	printf("stylee=%i\n",stylee);
-
-  if ( get_xvisinfo_class(vis) % 2 == 1) {	/* The odd numbers can redefine colors */
-
-			// printf("%i\n",get_xvisinfo_class(vis));
-		
-    colormap = DefaultColormap (d, screen);
-		Visual *defaultVisual=DefaultVisual(d,screen);
-		
-    /* Allocate cells */
-    allocateOK = (XAllocColorCells (d, colormap, 1,
-							    NULL, 0, color, COLORS) != 0);
-    if (!allocateOK) {
-
-			colormap = XCreateColormap(d,win,defaultVisual,AllocNone);
-			
-    	redocolors();
-						
-		} else {
-			printf("Allocated OK\n");
-      // This doesn't work for installed colormap!
-
-      /* Modify the colorcells */
+			  colormap = XCreateColormap(d,win,defaultVisual,AllocNone);
+		  	
+    	  redocolors();
+					  	
+      }
+  
+		  // black = XBlackPixel(d,screen);
+		  // white = XWhitePixel(d,screen);
+  
+    } else if ( get_xvisinfo_class(vis) == TrueColor) {
+					  printf("TrueColor\n");
+      /* This will lookup the color and sets the xrgb[i].pixel value */
       for (i = 0; i < COLORS; i++)
-				xrgb[i].pixel = color[i];
-
-      XStoreColors (d, colormap, xrgb, COLORS);
-
+        XAllocColor (d, colormap, &xrgb[i]);
+    } else
+      fprintf (stderr, "Not content with visual class %d.\n",
+	       get_xvisinfo_class(vis) ), exit (1);
+  
+    /* Find out if MITSHM is supported and useable */
+    printf ("MITSHM: ");
+  
+    if (XShmQueryVersion (d, &mitshm_major_code,
+			  &mitshm_minor_code, &shared_pixmaps)) {
+      int (*handler) (Display *, XErrorEvent *);
+      ximg = XShmCreateImage (d, vis.visual,
+			     vis.depth, XShmPixmapFormat (d),
+			     NULL, &shminfo, WIN_W, WIN_H);
+      shminfo.shmid = shmget (IPC_PRIVATE,
+			      ximg->bytes_per_line * ximg->height,
+			      IPC_CREAT | 0777);
+      shminfo.shmaddr = (char *)shmat (shminfo.shmid, 0, 0);
+		  ximg->data = (char *)shminfo.shmaddr;
+  
+      handler = XSetErrorHandler (mitshm_handler);
+      XShmAttach (d, &shminfo);	/* Tell the server to attach */
+      XSync (d, 0);
+      XSetErrorHandler (handler);
+  
+      shmctl (shminfo.shmid, IPC_RMID, 0);
+      /* Mark this shm segment for deletion at once. The segment will
+       * automatically become released when both the server and this
+       * client have detached from it.
+       * (Process termination automagically detach shm segments) */
+  
+      if (!can_use_mitshm) {
+        shmdt (shminfo.shmaddr);
+        ximg = NULL;
+      }
     }
-
-		// black = XBlackPixel(d,screen);
-		// white = XWhitePixel(d,screen);
-
-  } else if ( get_xvisinfo_class(vis) == TrueColor) {
-					printf("TrueColor\n");
-    /* This will lookup the color and sets the xrgb[i].pixel value */
-    for (i = 0; i < COLORS; i++)
-      XAllocColor (d, colormap, &xrgb[i]);
-  } else
-    fprintf (stderr, "Not content with visual class %d.\n",
-	     get_xvisinfo_class(vis) ), exit (1);
-
-  /* Find out if MITSHM is supported and useable */
-  printf ("MITSHM: ");
-
-  if (XShmQueryVersion (d, &mitshm_major_code,
-			&mitshm_minor_code, &shared_pixmaps)) {
-    int (*handler) (Display *, XErrorEvent *);
-    ximg = XShmCreateImage (d, vis.visual,
-			   vis.depth, XShmPixmapFormat (d),
-			   NULL, &shminfo, WIN_W, WIN_H);
-    shminfo.shmid = shmget (IPC_PRIVATE,
-			    ximg->bytes_per_line * ximg->height,
-			    IPC_CREAT | 0777);
-    shminfo.shmaddr = (char *)shmat (shminfo.shmid, 0, 0);
-		ximg->data = (char *)shminfo.shmaddr;
-
-    handler = XSetErrorHandler (mitshm_handler);
-    XShmAttach (d, &shminfo);	/* Tell the server to attach */
-    XSync (d, 0);
-    XSetErrorHandler (handler);
-
-    shmctl (shminfo.shmid, IPC_RMID, 0);
-    /* Mark this shm segment for deletion at once. The segment will
-     * automatically become released when both the server and this
-     * client have detached from it.
-     * (Process termination automagically detach shm segments) */
-
-    if (!can_use_mitshm) {
-      shmdt (shminfo.shmaddr);
-      ximg = NULL;
+  
+    if (ximg == NULL) {
+      can_use_mitshm = 0;
+      /* XInitImage(ximg); */
+      ximg = XCreateImage (d, vis.visual, vis.depth, ZPixmap,
+			  0, (char *)malloc (WIN_W * WIN_H), WIN_W, WIN_H, 8, 0);
     }
+  
+    if (can_use_mitshm)
+      printf ("YES!\n");
+    else
+      printf ("NO, using fallback instead.\n");
+  
+    // DrawFractal (ximg,xrgb);
+  
+    XSelectInput (d, win, ButtonPressMask | ExposureMask);
+    XMapWindow (d, win);
+  
+    printf ("Click in the fractal window to terminate the program.\n");
+  
+  
+  
+    real_main();
+
+
+  
+      // XNextEvent (d, &ev);
+      // switch (ev.type) {
+      // case ButtonPress:
+        // should_quit = 1;
+        // break;
+      // case Expose:
+        // if (can_use_mitshm)
+				  // XShmPutImage (d, win, gc, img, 0, 0, 0, 0, WIN_W, WIN_H, True);
+        // else
+				  // XPutImage (d, win, gc, img, 0, 0, 0, 0, WIN_W, WIN_H);
+        // break;
+      // default:
+			  // break;
+      // }
+  
+  
+  
+    if (get_xvisinfo_class(vis) % 2 == 1 || get_xvisinfo_class(vis) == TrueColor) {
+      unsigned long color[COLORS];
+  
+      if (allocateOK) {
+        for (i = 0; i < COLORS; i++)
+				  color[i] = xrgb[i].pixel;
+        XFreeColors (d, colormap, color, COLORS, 0);
+      }				/* Allocated colors freed */
+    } else {
+      XUninstallColormap (d, colormap);
+    }
+  
+    if (can_use_mitshm) {
+      XShmDetach (d, &shminfo);	/* Server detached */
+      XDestroyImage (ximg);	/* Image struct freed */
+      shmdt (shminfo.shmaddr);	/* We're detached */
+    } else
+      XDestroyImage (ximg);	/* Image struct freed */
+  
+    XDestroyWindow (d, win);	/* Window removed */
+    XCloseDisplay (d);		/* Display disconnected */
+  
+    /* So you can see how your computer compares to your friend's */
+    getrusage (RUSAGE_SELF, &resource_utilization);
+    printf ("CPU seconds consumed: %ds and %dµs\n",
+	    (int) resource_utilization.ru_utime.tv_sec,
+	    (int) resource_utilization.ru_utime.tv_usec);
+  
+    return 0;
   }
-
-  if (ximg == NULL) {
-    can_use_mitshm = 0;
-    /* XInitImage(ximg); */
-    ximg = XCreateImage (d, vis.visual, vis.depth, ZPixmap,
-			0, (char *)malloc (WIN_W * WIN_H), WIN_W, WIN_H, 8, 0);
-  }
-
-  if (can_use_mitshm)
-    printf ("YES!\n");
-  else
-    printf ("NO, using fallback instead.\n");
-
-  // DrawFractal (ximg,xrgb);
-
-  XSelectInput (d, win, ButtonPressMask | ExposureMask);
-  XMapWindow (d, win);
-
-  printf ("Click in the fractal window to terminate the program.\n");
-
-
-
-  real_main();
-
-    // XNextEvent (d, &ev);
-    // switch (ev.type) {
-    // case ButtonPress:
-      // should_quit = 1;
-      // break;
-    // case Expose:
-      // if (can_use_mitshm)
-				// XShmPutImage (d, win, gc, img, 0, 0, 0, 0, WIN_W, WIN_H, True);
-      // else
-				// XPutImage (d, win, gc, img, 0, 0, 0, 0, WIN_W, WIN_H);
-      // break;
-    // default:
-			// break;
-    // }
-
-
-
-  if (get_xvisinfo_class(vis) % 2 == 1 || get_xvisinfo_class(vis) == TrueColor) {
-    unsigned long color[COLORS];
-
-    if (allocateOK) {
-      for (i = 0; i < COLORS; i++)
-				color[i] = xrgb[i].pixel;
-      XFreeColors (d, colormap, color, COLORS, 0);
-    }				/* Allocated colors freed */
-  } else {
-    XUninstallColormap (d, colormap);
-  }
-
-  if (can_use_mitshm) {
-    XShmDetach (d, &shminfo);	/* Server detached */
-    XDestroyImage (ximg);	/* Image struct freed */
-    shmdt (shminfo.shmaddr);	/* We're detached */
-  } else
-    XDestroyImage (ximg);	/* Image struct freed */
-
-  XDestroyWindow (d, win);	/* Window removed */
-  XCloseDisplay (d);		/* Display disconnected */
-
-  /* So you can see how your computer compares to your friend's */
-  getrusage (RUSAGE_SELF, &resource_utilization);
-  printf ("CPU seconds consumed: %ds and %dµs\n",
-	  (int) resource_utilization.ru_utime.tv_sec,
-	  (int) resource_utilization.ru_utime.tv_usec);
-
-  return 0;
-}
-
+  
 
 
 
