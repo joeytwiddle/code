c158ccdf358056ffbdec69c768fcd9812521a96b
joey <joey>
2001-10-19 18:06:34 +0000

    *** empty log message ***

--- c/phd/projprof/procres/finderr.c
+++ c/phd/projprof/procres/finderr.c
@@ -51,16 +51,17 @@ main(int argc,String *argv) {
   a.done();
 
 	List<String> lines=readlinesfromfile(filename);
+	int lastyaw=-1;
 	int i=0;
 	String l=lines.get(i);
 	if (!Seq(l,"START"))
-		printf("Error 0 line %i\n",i);
+		fprintf(stderr,"Error 0 line %i\n",i);
 	else
 		i++;
 	while (i<lines.len) {
 		l=lines.get(i);
 		if (!Seq(l,"SIM:")) {
-			// printf("Error A line %i\n",i);
+			fprintf(stderr,"Error A line %i - seeking\n",i);
 			break;
 		}	else {
 		  float roll,yaw,pitch;
@@ -83,35 +84,74 @@ main(int argc,String *argv) {
 		  // printf("Got %s for GTd\n",gtdown.toString());
 		  i++; l=lines.get(i);
 		  if (!Seq(l,"PP:"))
-			  printf("Error B line %i\n",i);
+			  fprintf(stderr,"Error B line %i\n",i);
 		  i++; l=lines.get(i);
+			bool bad=false;
 		  if (Seq(l,"")) {
-			  // printf("Error D line %i\n",i);
+				fprintf(stderr,"no pp res r%i-y%i-p%i\n",(int)roll,(int)yaw,(int)pitch);
+			  fprintf(stderr,"  Error D line %i\n",i);
+				ppright=gtright;
+				ppdown=gtdown;
+				bad=true;
 		  } else {
 			  sscanf(l,"right = (%f,%f,%f)",&ppright.x,&ppright.y,&ppright.z);
 			  // printf("Got %s for GTr\n",ppright.toString());
 			  i++; l=lines.get(i);
 			  sscanf(l,"down = (%f,%f,%f)",&ppdown.x,&ppdown.y,&ppdown.z);
 			  // printf("Got %s for GTr\n",ppdown.toString());
-			  float corra=V3d::normdot(gtright,ppright);
-			  float corrb=V3d::normdot(gtdown,ppdown);
-			  // printf("Correlation: %f %f\n",corra,corrb);
-			  // printf("%f %f %f\n",yaw,pitch,myabs(corra*corrb));
-			  // printf("%f %f %f\n",yaw,pitch,corra*corrb);
-			  // printf("%f %f %f %f\n",yaw,pitch,corra,corrb);
-			  // printf("%f %f %f %f %f\n",yaw,pitch,(myabs(corra*corrb)+1.0)/2.0,corra,corrb);
-			  printf("%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n",
-					yaw,pitch,corra*corrb,corra,corrb,
-					gtright.x,gtright.y,gtright.z,ppright.x,ppright.y,ppright.z,
-					gtdown.x,gtdown.y,gtdown.z,ppdown.x,ppdown.y,ppdown.z
-				);
-			  i++; l=lines.get(i);
-		  }
+		    i++; l=lines.get(i);
+			}
+			
+			float corra=-V3d::normdot(gtright,ppright);
+		  float corrb=-V3d::normdot(gtdown,ppdown);
+			float acc=corra*corrb;
+			V3d gtnor=V3d::normcross(gtright,gtdown);
+			V3d ppnor=V3d::normcross(ppright,ppdown);
+			float altacc=V3d::normdot(gtnor,ppnor);
+			float faceonness=V3d::normdot(gtnor,V3d::k);
+			float maxangle=180*acos(faceonness)/pi;
+
+			if (!bad) {
+				if (corra<0 && corrb<0) {
+					fprintf(stderr,"  double negative correl r%i-y%i-p%i\n",(int)roll,(int)yaw,(int)pitch);
+			  } else if (corra<0 || corrb<0) {
+				  fprintf(stderr,"  single negative correl r%i-y%i-p%i\n",(int)roll,(int)yaw,(int)pitch);
+				  altacc=-3;
+				}
+			}
+		  
+			// printf("Correlation: %f %f\n",corra,corrb);
+		  // printf("%f %f %f\n",yaw,pitch,myabs(corra*corrb));
+		  // printf("%f %f %f\n",yaw,pitch,corra*corrb);
+		  // printf("%f %f %f %f\n",yaw,pitch,corra,corrb);
+		  // printf("%f %f %f %f %f\n",yaw,pitch,(myabs(corra*corrb)+1.0)/2.0,corra,corrb);
+			
+			if (yaw<lastyaw)
+				printf("\n"); // Seperate blocks for grid plot
+			lastyaw=yaw;
+			
+			// This is no longer cheating but it lets us see real errors
+			// if (acc<0)
+				// acc=1;
+			// if (altacc<0)
+				// altacc=1;
+
+			if (bad) {
+				acc=-2; altacc=-2;
+			}
+			
+		  printf("%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n",
+				yaw,pitch,acc,corra,corrb,
+				gtright.x,gtright.y,gtright.z,ppright.x,ppright.y,ppright.z,
+				gtdown.x,gtdown.y,gtdown.z,ppdown.x,ppdown.y,ppdown.z,
+				altacc,faceonness,maxangle
+			);
+		  
 		  if (!Seq(l,""))
-			  printf("Error C line %i\n",i);
+			  fprintf(stderr,"Error C line %i\n",i);
 		  i++;
 		}
 	}
-	printf("1 yaw     2 roll    3 acc     4 racc   5 dacc     6 gtrx    7 gtry    8 gtrz    9 pprx    10 ppry    11 pprz    12 gtdx    13 gtdy   14 gtdz   15 ppdx   16 ppdy   17 ppdz\n");
+	printf("\n# 1 yaw     2 pitch   3 acc     4 racc   5 dacc     6 gtrx    7 gtry    8 gtrz    9 pprx    10 ppry    11 pprz    12 gtdx    13 gtdy   14 gtdz   15 ppdx   16 ppdy   17 ppdz   18 altacc  19 faceonness 20 maxang\n");
 	
 }
