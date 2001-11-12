99ad91ac6e60e935ed6a6d0e7adec3d895a6f8a1
joey <joey>
2001-11-12 17:18:26 +0000

    *** empty log message ***

--- c/phd/projprof/pp.c
+++ c/phd/projprof/pp.c
@@ -289,7 +289,7 @@ public:
       for (int j=0;j<h;j++) {
         if (binimg.getpos(i,j)) {
           if (myrnd()>prob) {
-            binimg.setpos(i,j,false);
+            // binimg.setpos(i,j,false);
           } else {
             V2d pos=V2d(i,j);
 						VP *vp;
