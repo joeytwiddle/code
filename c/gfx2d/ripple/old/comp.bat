c73b0a54b37fe3918fc5817b0dbedffeea1624ee
joey <joey>
2001-11-12 14:30:52 +0000

    *** empty log message ***

--- c/gfx2d/ripple/old/comp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem gcc current.c c:\joey\djgpp\contrib\grx23\lib\dj2\libgrx20.a -lm -g
-gcc current.c c:\joey\code\djgpp\contrib\grx23\lib\dj2\libgrx20.a -lm -g -o go.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-if not errorlevel 0 a>tmptype tmp
--- c/gfx2d/ripple/old/comp2.bat
+++
@@ -1,1 +0,0 @@
-@gcc colorops.c c:\joey\djgpp\contrib\grx23\lib\dj2\libgrx20.a
