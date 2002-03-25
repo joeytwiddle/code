f3e0abc692ac6584b1d061b491e8047ec097bd9c
joey <joey>
2002-03-25 00:45:19 +0000

    *** empty log message ***

--- c/gfx/sdl/transtri/transtri.c
+++ c/gfx/sdl/transtri/transtri.c
@@ -1,56 +1,66 @@
-// todo: Efficient interpolation (asm?) of BG (interpolate D,a space rather than color space?)
-// process: Any macros which use a provided expression N times
-//   should be turned into functions if, in terms of time,
-//   N*eval(expression) > 1*alloc(type)+1*eval(expression)+N*refer(type)
-//   (This will be done like BR and TR I guess.)
-//   Or does the C compiler do this automatically, assuming state of
-//   expression does not change during macro.
-//   Benchmark test?
-// todo: Resolve palette==NULL at init and switch routing of code to be execed.
-//   Or make it part of the SDLwrap model.
-//   Or just ignore it because we will only stand for truecolour.
-// todo: Both BR and TR should be given functions to perform on the pixel register
-//   rather than x,y.  This will allow the algorithms to perform fast
-//   reg++'s rather than the given function performing reg=big_calculation(x,y)
-//   (Actually keep passing x,y just in case the plot function wants to use it.)
-//   [ 19/3/02 Attempted for TR ]
-// process: Are we ever forced to blit on a 16bit surface?
-//   If so, would it be efficient to use a hidden 32bit surface to do
-//   read/writes for transparency, and then blit via the 16bit surface?
-// process: Is there in general a better local surface we can use for lookups
-//   before finally writing to the screen?
-// process: When rendering two overlapping triangles, the result is three
-//   polygons: two containing the pure colours, and one the merged colour.
-//   We can avoid reading all the pixels from the map if we find the triangle
-//   intersections first, and then render the resulting polygons with
-//   the merged colours pre-calculated.
-//   (In fact only one pixel read is needed, for the background.)
-//   This requires 2D "CSG".
-// todo: Of course, stop using full colour and start cheating.
-//   eg. Use transparencies of one colour (essentially greyscale),
-//   or OR otherwise limit bits as Julian suggests.
-// todo: Store #define constants for a variety of video modes so that the
-//   user may choose at runtime.  (erm what? how?!)
+/*
+done: Efficient interpolation (asm?) of BG (interpolate D,a space rather than color space?)
+      (Interpolation done but not in asm)
+      A better description is to say we are interpolating in the image plane
+      using bgtexture position, rather than colour
+      It means we can do less work or plot pixels more correctly for the same amount of work.
+done: Interpolate inter-pixel on the bgtexture plane. Slow.
+process: Any macros which use a provided expression N times
+  should be turned into functions if, in terms of time,
+  N*eval(expression) > 1*alloc(type)+1*eval(expression)+N*refer(type)
+  (This will be done like BR and TR I guess.)
+  Or does the C compiler do this automatically, assuming state of
+  expression does not change during macro.
+  Benchmark test?
+todo: Resolve palette==NULL at init and switch routing of code to be execed.
+  Or make it part of the SDLwrap model.
+  Or just ignore it because we will only stand for truecolour.
+halfdone: Both BR and TR should be given functions to perform on the pixel register
+  rather than x,y.  This will allow the algorithms to perform fast
+  reg++'s rather than the given function performing reg=big_calculation(x,y)
+  (Actually keep passing x,y just in case the plot function wants to use it.)
+  [ 19/3/02 Attempted for TR ]
+process: Are we ever forced to blit on a 16bit surface?
+  If so, would it be efficient to use a hidden 32bit surface to do
+  read/writes for transparency, and then blit via the 16bit surface?
+process: Is there in general a better local surface we can use for lookups
+  before finally writing to the screen?
+process: When rendering two overlapping triangles, the result is three
+  polygons: two containing the pure colours, and one the merged colour.
+  We can avoid reading all the pixels from the map if we find the triangle
+  intersections first, and then render the resulting polygons with
+  the merged colours pre-calculated.
+  (In fact only one pixel read is needed, for the background.)
+  This requires 2D "CSG".
+todo: Of course, stop using full colour and start cheating.
+  eg. Use transparencies of one colour (essentially greyscale),
+  or OR otherwise limit bits as Julian suggests.
+todo: Store #define constants for a variety of video modes so that the
+  user may choose at runtime.  (erm what? how?!)
+*/
 
 // #define DOS
 // #define DO_FULLSCREEN
-#define SCRWID 600
-#define SCRHEI 400
-#define NUMTRIS 400
-#define brightness 8
-// #define SCRWID 640
-// #define SCRHEI 480
-// #define NUMTRIS 100
-// #define brightness 30
+// #define SCRWID 320
+// #define SCRHEI 200
+// #define NUMTRIS 400
+// #define brightness 8
+#define SCRWID 640
+#define SCRHEI 480
+#define NUMTRIS 100
+#define brightness 30
 #define SCRBPS 32
 #define desiredFramesPerSecond 50
 #define IMGSKIP (SCRHEI/40)
 // #define IMGSKIP 4
-// #define DOUBLEMERGEBG
 #define DO_TRIANGLES
 // #define DO_STARS
 #define DO_WHITEOUT
 #define BGQUANT 8
+// #define INTERPOLATE_BG_FULL
+#define ROTSYM180
+// #define POSTERISE
+#define TRISINGOFFSET 16
 
 #ifdef DOS
 #define M_PI 3.14159265358979323846
@@ -181,29 +191,30 @@ plotLine(screen,TR_x2,TR_y2,TR_x3,TR_y3,MapRGB(screen->format,255,255,255));
 		bluereg=(bluereg<255-db?bluereg+db:255);
 	}
 
-// inline screen_pixelType merge_pixel(screen_pixelType p,Uint8 dr,Uint8 dg,Uint8 db) {
-// Uint8 r,g,b;
-// Uint16 fr,fg,fb;
-// screen_GetRGB(p,&r,&g,&b);
-// // printf("    %i %i %i\n",r,g,b);
-// // printf("  + %i %i %i\n",dr,dg,db);
-// fr=r+dr;
-// fg=g+dg;
-// fb=b+db;
-// // note: Sum of A B will exceed 255 if from top bit down either both bits are set or one bit is set, check next (worst case 00000001+11111111)
-// // process: In asm, don't we get a register bit set if there was an overflow?
-// // p= SDL_MapRGB(screen->format,fr,fg,fb);
-// return screen_MapRGB(fr>255?255:fr,fg>255?255:fg,fb>255?255:fb);
-// }
-// 
-// inline void merge(SDL_Surface *screen,int x,int y,Uint8 dr,Uint8 dg,Uint8 db) {
-// // Uint32 p=getPixel(screen,x,y);
-// // Uint32 p=screen_getPixel(x,y);
-// screen_pixelType p=screen_getPixel(x,y);
-// // printf("%i\n",p);
-// // SDL_GetRGB(p,(screen->format),&r,&g,&b);
-// screen_setPixel(x,y,merge_pixel(p,dr,dg,db));
-// }
+/* inline screen_pixelType merge_pixel(screen_pixelType p,Uint8 dr,Uint8 dg,Uint8 db) {
+Uint8 r,g,b;
+Uint16 fr,fg,fb;
+screen_GetRGB(p,&r,&g,&b);
+// printf("    %i %i %i\n",r,g,b);
+// printf("  + %i %i %i\n",dr,dg,db);
+fr=r+dr;
+fg=g+dg;
+fb=b+db;
+// note: Sum of A B will exceed 255 if from top bit down either both bits are set or one bit is set, check next (worst case 00000001+11111111)
+// process: In asm, don't we get a register bit set if there was an overflow?
+// p= SDL_MapRGB(screen->format,fr,fg,fb);
+return screen_MapRGB(fr>255?255:fr,fg>255?255:fg,fb>255?255:fb);
+}
+
+inline void merge(SDL_Surface *screen,int x,int y,Uint8 dr,Uint8 dg,Uint8 db) {
+// Uint32 p=getPixel(screen,x,y);
+// Uint32 p=screen_getPixel(x,y);
+screen_pixelType p=screen_getPixel(x,y);
+// printf("%i\n",p);
+// SDL_GetRGB(p,(screen->format),&r,&g,&b);
+screen_setPixel(x,y,merge_pixel(p,dr,dg,db));
+}
+*/
 
 #define halfClipLine(XA,YA,XB,YB,LX,HX,LY,HY) { if (XA<LX) { YA = YA + (YB-YA)*(LX-XA)/(XB-XA); XA = LX; } if (XA>=HX) { YA = YA + (YB-YA)*(HX-XA)/(XB-XA); XA = HX-1; } if (YA<LY) { XA = XA + (XB-XA)*(LY-YA)/(YB-YA); YA = LY; } if (YA>=HY) { XA = XA + (XB-XA)*(HY-YA)/(YB-YA); YA = HY-1; } }
 // #define halfClipLine(XA,YA,XB,YB,LX,HX,LY,HY) { if ((*(XA))<LX) { (*(YA)) = (*(YA)) + ((*(YB))-(*(YA)))*(LX-(*(XA)))/((*(XB))-(*(XA))); (*(XA)) = LX; } if ((*(XA))>=HX) { (*(YA)) = (*(YA)) + ((*(YB))-(*(YA)))*(HX-(*(XA)))/((*(XB))-(*(XA))); (*(XA)) = HX-1; } if ((*(YA))<LY) { (*(XA)) = (*(XA)) + ((*(XB))-(*(XA)))*(LY-(*(YA)))/((*(YB))-(*(YA))); (*(YA)) = LY; } if ((*(YA))>=HY) { (*(XA)) = (*(XA)) + ((*(XB))-(*(XA)))*(HY-(*(YA)))/((*(YB))-(*(YA))); (*(YA)) = HY-1; } }
@@ -324,7 +335,7 @@ int main(int argc,char *argv[]) {
 	while (keepLooping) {
 
 		int releasetime = clock()+CLOCKS_PER_SEC/desiredFramesPerSecond;
-		framebits=((float)clock()-start_clock)*5.0/CLOCKS_PER_SEC;
+		framebits=((float)clock()-start_clock)*20.0/CLOCKS_PER_SEC;
 
 		// printf("%i %i %li\n",frames,framebits,CLOCKS_PER_SEC);
 
@@ -351,10 +362,6 @@ int main(int argc,char *argv[]) {
 			// Uint32 tmp=((Uint16)bgtexture_w << 8)/M_PI;
 			// Sint16 D2offset=Doffset+400.0*qsin(framebits*M_PI/140.0+123.6);
 			// Sint16 a2offset=aoffset+100.0*qsin(framebits*M_PI/140.0+123.6);
-#ifdef DOUBLEMERGEBG
-			Sint16 D2offset=1234+Doffset+framebits*4.0+200.0*sin(framebits*M_PI/123.0);
-			Sint16 a2offset=4321+aoffset+0.4*framebits+20.0*sin(framebits*M_PI/453.0+1.2);
-#endif
 			SDL_Rect dstrect;
 			dstrect.w=IMGSKIP;
 			dstrect.h=IMGSKIP;
@@ -386,7 +393,12 @@ int main(int argc,char *argv[]) {
 #define D ( d<2 ? 0 : overd/d )
 					// fprintf(output,"  %i\n",D);
 					// Uint16 a=(Uint16)((((Uint32)(tmp*(atan2(i-cylcenx,j-cylceny))) >> 8)+M_PI)+aoffset);
-#define a ( BGQUANT*bgtexture_w * ( atan2(i-cylcenx,j-cylceny ) / M_PI + BGQUANT*M_PI ) )
+#ifdef ROTSYM180
+					#define BGrotscale 1
+#else
+					#define BGrotscale 0.5
+#endif
+#define a ( BGQUANT * (bgtexture_w * BGrotscale * ( atan2(i-cylcenx,j-cylceny ) / M_PI + M_PI ) ) )
 					// dstrect.y=j;
 
 #ifdef DO_WHITEOUT
@@ -394,8 +406,15 @@ int main(int argc,char *argv[]) {
 					brmax=255-br;
 #endif
 
-					anow=((Uint16)(a+aoffset));
+					anow=((Uint16)(a+aoffset))%(bgtexture_w*BGQUANT);
 					Dnow=((Uint16)(D+Doffset));
+
+#ifdef POSTERISE
+					p=bgpixels[((anow/BGQUANT)%bgtexture_w)+((Dnow/BGQUANT)%bgtexture_h)*bgtexture_w];
+					if (i<screen_w && j<screen_h) {
+					SDL_FillRect(screen,&dstrect,p);
+					}
+#else
 					if (j>0) {
 						if (i>0) {
 							Uint16 TLx=as[j-IMGSKIP];
@@ -407,14 +426,43 @@ int main(int argc,char *argv[]) {
 							Uint16 BRx=anow;
 							Uint16 BRy=Dnow;
 							// Hack around problem interpolating over TLa=359 TRa=001
-							if (i-cylcenx>=0 && i-cylcenx<IMGSKIP && j-cylceny<0) {
-								if (TRx-TLx>BGQUANT*bgtexture_w/2 || BRx-TLx>BGQUANT*bgtexture_w/2) {
-									TLx+=BGQUANT*bgtexture_w*2;
-									// }
-								// if (BRx-BLx>BGQUANT*bgtexture_w/2 || TRx-BLx>BGQUANT*bgtexture_w/2) {
-								BLx+=BGQUANT*bgtexture_w*2;
-							}
-							}
+							// Doesn't smegging work
+							// Ooh hurrah it smegging does now
+							if ( TRx>BGQUANT*bgtexture_w*3/4 || BRx>BGQUANT*bgtexture_w*3/4 ||
+									TLx>BGQUANT*bgtexture_w*3/4 || BLx>BGQUANT*bgtexture_w*3/4 ) {
+								if (TLx<BGQUANT*bgtexture_w/4)
+									TLx+=BGQUANT*bgtexture_w;
+								if (TRx<BGQUANT*bgtexture_w/4)
+									TRx+=BGQUANT*bgtexture_w;
+								if (BLx<BGQUANT*bgtexture_w/4)
+									BLx+=BGQUANT*bgtexture_w;
+								if (BRx<BGQUANT*bgtexture_w/4)
+									BRx+=BGQUANT*bgtexture_w;
+							} 
+							// This doesn't appear to prevent the odd glitch, so I've taken it out
+							// The glitches remain :-(
+							// Oh they have hopefully gone now, I found a mistake.
+							// It would appear not.
+							if ( TRy>BGQUANT*bgtexture_h*3/4 || BRy>BGQUANT*bgtexture_h*3/4 ||
+									TLy>BGQUANT*bgtexture_h*3/4 || BLy>BGQUANT*bgtexture_h*3/4 ) {
+								if (TLy<BGQUANT*bgtexture_h/4)
+									TLy+=BGQUANT*bgtexture_h;
+								if (TRy<BGQUANT*bgtexture_h/4)
+									TRy+=BGQUANT*bgtexture_h;
+								if (BLy<BGQUANT*bgtexture_h/4)
+									BLy+=BGQUANT*bgtexture_h;
+								if (BRy<BGQUANT*bgtexture_h/4)
+									BRy+=BGQUANT*bgtexture_h;
+							} 
+							// Worked when BGQUANT=1, but not sure why bgtexture_w*2
+							// if (i-cylcenx>=0 && i-cylcenx<IMGSKIP && j-cylceny<0) { Does not apply if BGQUANT!=1, besides no speed gain!
+							// if (TRx-TLx>BGQUANT*bgtexture_w/2 || BRx-TLx>BGQUANT*bgtexture_w/2) {
+							// TLx+=BGQUANT*bgtexture_w*2;
+							// // }
+							// // if (BRx-BLx>BGQUANT*bgtexture_w/2 || TRx-BLx>BGQUANT*bgtexture_w/2) {
+							// BLx+=BGQUANT*bgtexture_w*2;
+							// }
+							// }
 							{
 								register Uint8 I,J;
 								register screen_pixelType *reg;
@@ -431,9 +479,29 @@ int main(int argc,char *argv[]) {
 #define s ((J))
 										register Uint8 w=((IMGSKIP-I));
 										register Uint8 n=((IMGSKIP-J));
-										ahere=(TLx*n*w+TRx*n*e+BLx*s*w+BRx*s*e)/IMGSKIP/IMGSKIP;
-										Dhere=(TLy*n*w+TRy*n*e+BLy*s*w+BRy*s*e)/IMGSKIP/IMGSKIP;
+										ahere=((TLx*n*w+TRx*n*e+BLx*s*w+BRx*s*e)/IMGSKIP/IMGSKIP)%(bgtexture_w*BGQUANT);
+										Dhere=((TLy*n*w+TRy*n*e+BLy*s*w+BRy*s*e)/IMGSKIP/IMGSKIP)%(bgtexture_h*BGQUANT);
+#ifdef INTERPOLATE_BG_FULL
+										{
+											Uint16 iahere=((ahere/BGQUANT)%bgtexture_w);
+											Uint16 iDhere=((Dhere/BGQUANT)%bgtexture_h);
+											Uint16 ianext=(iahere+1)%bgtexture_w;
+											Uint16 iDnext=(iDhere+1)%bgtexture_h;
+#define NWp (iahere+bgtexture_w*iDhere)
+#define NEp (ianext+bgtexture_w*iDhere)
+#define SWp (iahere+bgtexture_w*iDnext)
+#define SEp (ianext+bgtexture_w*iDnext)
+#define east (ahere-iahere*BGQUANT)
+#define south (Dhere-iDhere*BGQUANT)
+#define west (BGQUANT-east)
+#define north (BGQUANT-south)
+											((Uint8 *)&p)[0]=((Uint32)((Uint8 *)&bgpixels[NWp])[0]*north*west+((Uint8 *)&bgpixels[NEp])[0]*north*east+((Uint8 *)&bgpixels[SWp])[0]*south*west+((Uint8 *)&bgpixels[SEp])[0]*south*east)/BGQUANT/BGQUANT;
+											((Uint8 *)&p)[1]=((Uint32)((Uint8 *)&bgpixels[NWp])[1]*north*west+((Uint8 *)&bgpixels[NEp])[1]*north*east+((Uint8 *)&bgpixels[SWp])[1]*south*west+((Uint8 *)&bgpixels[SEp])[1]*south*east)/BGQUANT/BGQUANT;
+											((Uint8 *)&p)[2]=((Uint32)((Uint8 *)&bgpixels[NWp])[2]*north*west+((Uint8 *)&bgpixels[NEp])[2]*north*east+((Uint8 *)&bgpixels[SWp])[2]*south*west+((Uint8 *)&bgpixels[SEp])[2]*south*east)/BGQUANT/BGQUANT;
+										}
+#else
 										p=bgpixels[((ahere/BGQUANT)%bgtexture_w)+((Dhere/BGQUANT)%bgtexture_h)*bgtexture_w];
+#endif
 
 										*reg=p;
 #ifdef DO_WHITEOUT
@@ -466,8 +534,6 @@ int main(int argc,char *argv[]) {
 								// screen_setPixel(i,j,screen_MapRGB(thruwid,j*256/screen_h,qsi));
 								// p=screen_MapRGB(r,g,b);
 
-								// SDL_FillRect(screen,&dstrect,p);
-
 									}
 									startreg+=screen_w;
 								}
@@ -483,6 +549,7 @@ int main(int argc,char *argv[]) {
 					}
 					Dlast=Dnow;
 					alast=anow;
+#endif
 				}
 			}
 
@@ -491,7 +558,7 @@ int main(int argc,char *argv[]) {
 			for (i=0;i<NUMTRIS;i++) {
 				// plotLine(screen,screen_w*frand(),screen_h*frand(),scr een_w*frand(),screen_h*frand(),MapRGB(screen->format,256*frand(),256*frand(),256*frand()));
 				// plotTri(screen,screen_w*frand(),screen_h*frand(),screen_w*frand(),screen_h*frand(),screen_w*frand(),screen_h*frand(),MapRGB(screen->format,256*frand(),256*frand(),256*frand()));
-				plotTriRGB(screen,screen_w*nearhalf(),screen_h*nearhalf(),screen_w*nearhalf(),screen_h*nearhalf(),screen_w*nearhalf(),screen_h*nearhalf(),brightness*nearhalf(),brightness*nearhalf(),brightness*nearhalf());
+				plotTriRGB(screen,-cylcenx/TRISINGOFFSET+screen_w*nearhalf(),-cylceny/TRISINGOFFSET+screen_h*nearhalf(),-cylcenx/TRISINGOFFSET+screen_w*nearhalf(),-cylceny/TRISINGOFFSET+screen_h*nearhalf(),-cylcenx/TRISINGOFFSET+screen_w*nearhalf(),-cylceny/TRISINGOFFSET+screen_h*nearhalf(),brightness*nearhalf(),brightness*nearhalf(),brightness*nearhalf());
 			}
 
 #endif
