a778a55ba9728f4c7d6d9c8c4f70ef783561bd41
joey <joey>
2002-05-20 02:46:24 +0000

    *** empty log message ***

---
+++ c/phd/gentestimg/shownicefit.sh
@@ -0,0 +1,34 @@
+# killall ghostview
+
+rm *.eps
+
+COM="./simgen
+-yoff 0.01 -depth 0.5
+-lines 14 -roll 0 -yaw 0 -pitch 30 -size 0.4
+-qnd -image -overlay
+simgs/dar-crpage2.bmp tmp.bmp
+-imgnoise 0.0
+-noise 0.2
+-gnuplot
+-showexp
+-nornd
+$@";
+# -ins 1
+# -rems 1
+# -focal 0.1
+
+for F in `seq -w 0.5 0.3 4.0`; do
+# for X in "" "-spacings"; do
+# for Y in "" "-noransac"; do
+for X in ""; do
+for Y in ""; do
+	echo `curseyellow`$COM`cursegrey`
+	echo "set title \"$X $Y, f.p. $F\" \"Times-Roman,26\"" > title.dogpl
+	$COM $X $Y -focal $F > simgen.out
+	cp gplfit.ps gplfit$X$Y$F.eps
+	# gv gplfit$X$Y.eps
+done
+done
+done
+
+convert -loop 0 -delay 100 -geom 400 gplfit*.eps anim.gif
