99f7d281a9c79a9d766bdb7834cec474e11ebe00
joey <joey>
2002-06-06 01:01:25 +0000

    *** empty log message ***

--- dosbats/java.bat
+++ dosbats/java.bat
@@ -1,2 +1,2 @@
-@c:\install\javasoft\jre\1.3.1\bin\java %1 %2 %3 %4 %5 %6 %7
-rem @c:\jdk1.2\bin\java %1 %2 %3 %4 %5 %6 %7
+@e:\progra~1\javasoft\jre\1.3.1\bin\java %1 %2 %3 %4 %5 %6 %7
+@rem @c:\jdk1.2\bin\java %1 %2 %3 %4 %5 %6 %7
