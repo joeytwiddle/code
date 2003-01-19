af2ebcef691370cfaa13532eba02b6e692ef628a
joey <joey>
2003-01-19 07:18:22 +0000

    *** empty log message ***

--- haskell/tools/treelist.hs
+++ haskell/tools/treelist.hs
@@ -5,19 +5,20 @@ main = do
   args <- getArgs
   go args
 
-data Tree = Branch [Tree] | Leaf [String]
+data Tree = Branch String [Tree] | Leaf [String]
   -- deriving (Show)
 
 instance Show Tree where
-  show (Branch bs) = concat (map (showTree "") bs)
+  show (Branch c bs) = showTree "" (Branch c bs)
   -- show (Leaf ss) = concat (map addn ss)
     -- where addn s = s++"\n"
 
-showTree indent (Branch bs) = concat (map (showTree (indent++" |- ")) bs)
+showTree indent (Branch c bs) = indent++""++c++"\n"++ concat (map (showTree (indent++replicate (length c) ' ')) bs) ++ indent++"}\n"
 showTree indent (Leaf ls) = concat (map (sort) ls)
   where sort xs = indent ++ xs ++ "\n"
 
-test = go ["/stuff/data/cdrom_a2.find"]
+-- test = go ["/stuff/data/cdrom_a2.find"]
+test = go ["test.txt"]
 
 go args = do
           file <- readFile (head args)
@@ -28,10 +29,20 @@ treelist file = treebreak 0 (lines (file))
 treebreak :: Int -> [String] -> (Tree)
 treebreak col lines
   | indextoolarge = Leaf (concat breaks)
-  | (length breaks) == 1 = assert (breaks == [lines]) (treebreak (col+1) lines)
-  | otherwise = Branch ( map (treebreak (col+1)) breaks)
+  | (length breaks) == 1 = assert (breaks == [lines]) (Branch (take (col+1) (head (head breaks))) [(treebreak (0) (stripc (col+1) lines))])
+  | otherwise = Branch (take (col+1) (head (head breaks))) ( map (treebreak (0)) (strip (col+1) breaks))
   where breaks = findbreaks col [] lines
-        indextoolarge = col > (500)
+        indextoolarge = length lines == 1 -- col > 1 -- (length (head (head breaks))) -- col>2000 -- (myhead (head (head breaks))) == '_'
+        strip n xs = map (stripc n) xs
+        stripc n ys = map (drop n) ys
+{-++clip (show (map length breaks)))-}
+
+clip xs
+  | length xs < 50 = xs
+  | otherwise      = take 50 xs
+
+myheads [] = '_'
+myheads (h:t) = h
 
 assert x y = if x then y else error ( show x ++ "is false" )
 
