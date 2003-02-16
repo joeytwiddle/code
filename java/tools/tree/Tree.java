e17b6b0ae5caf9b1a9b303ec77ee7272e191c8d2
joey <joey>
2003-02-16 03:45:30 +0000

    awful result in vim since it quantises global indent levels, rather than local differences

--- java/tools/tree/Tree.java
+++ java/tools/tree/Tree.java
@@ -32,12 +32,9 @@ public class Tree {
 			while ( ( line = read.readLine() ) != null ) {
 
 				int same = countMatch(lastline,line);
-				if (same>lasti)
-					System.out.println("+ "+line.substring(0,same)+"{");
-				if (same<lasti)
-					// System.out.println("- "+lastline.substring(0,lasti)+"}");
-					System.out.println("- }");
-				System.out.println("."+same+" "+line);
+				for (int i=0;i<same;i++)
+					System.out.print(' ');
+				System.out.println(line.substring(same));
 				lastline = line;
 				lasti = same;
 
