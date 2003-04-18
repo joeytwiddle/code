800d873a1a8442303419bbbe1ba1ae2c65375734
joey <joey>
2003-04-18 16:32:29 +0000

    *** empty log message ***

--- c/tools/escapenewlines/compileandtest.sh
+++ c/tools/escapenewlines/compileandtest.sh
@@ -1,19 +1,19 @@
-TESTSRC=encodeslashn.c
-gcc encodeslashn.c -o encodeslashn &&
-gcc decodeslashn.c -o decodeslashn &&
-./encodeslashn $TESTSRC > test1.enc &&
-./decodeslashn test1.enc > test1.dec &&
-cat $TESTSRC | ./encodeslashn > test2.enc &&
-cat test2.enc | ./decodeslashn > test2.dec &&
+TESTSRC=escapenewlines.c
+gcc escapenewlines.c -o escapenewlines &&
+gcc unescapenewlines.c -o unescapenewlines &&
+./escapenewlines $TESTSRC > test1.escaped &&
+./unescapenewlines test1.escaped > test1.dec &&
+cat $TESTSRC | ./escapenewlines > test2.escaped &&
+cat test2.escaped | ./unescapenewlines > test2.dec &&
 cmp $TESTSRC test1.dec &&
 cmp $TESTSRC test2.dec &&
-! cmp $TESTSRC test1.enc > /dev/null &&
-! cmp $TESTSRC test2.enc > /dev/null &&
-test `countlines test1.enc` = 1 &&
-test `countlines test2.enc` = 1 &&
+! cmp $TESTSRC test1.escaped > /dev/null &&
+! cmp $TESTSRC test2.escaped > /dev/null &&
+test `countlines test1.escaped` = 1 &&
+test `countlines test2.escaped` = 1 &&
 (
 	echo "Compilation and tests went fine =) .  You may want to:"
-	echo ln -sf $PWD/encodeslashn $HOME/bin/encodeslashn
-	echo ln -sf $PWD/decodeslashn $HOME/bin/decodeslashn
+	echo ln -sf $PWD/escapenewlines $HOME/bin/escapenewlines
+	echo ln -sf $PWD/unescapenewlines $HOME/bin/unescapenewlines
 
 )
