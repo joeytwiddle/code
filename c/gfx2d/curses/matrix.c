b3f7ee4579c30f65772bb90bbc2f1c971a634a00
joey <joey>
2003-09-26 19:02:04 +0000

    *** empty log message ***

--- c/gfx2d/curses/matrix.c
+++ c/gfx2d/curses/matrix.c
@@ -1,10 +1,21 @@
-/** damnandblast.c
+/** matrix.c
  *  by joey
- *  No interpolation!
+**/
+
+/** TODO:
+ *  Altering happens too fast wrt sliding.  Run them in parallel.
+ *  Wider screen => more parallelisation.
+ *  Some kind of timing so that it doesn't go too fast on new machines.
+ *  Make the white things more like original (eg. changing symbol, sometimes static, ...).
+ *  In XMatrix, columns do not slide at different speeds!
+ *  Putty compatability?
+ *  Turn cursor off.
+ *  Exit cleanly on keypress.
 **/
 
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curses.h>
 
 void writeToPoint(int x,int y) {
@@ -31,8 +42,10 @@ void main() {
 
 	mbit** thematrix;
 	
-	mbit* palette = "^+oiumq/\\*0$%@#";
+	mbit* palette = "^+ouq/\\*}0&$@#";
 	int paletteSize = strlen(palette);
+	// mbit* altPalette = "%@#";
+	// int altPaletteSize = strlen(altPalette);
 	
 	srand(time(NULL));
 	// Needed
@@ -69,37 +82,36 @@ void main() {
 		}
 	}
 
-	int AVSLIDELEN = ( LINES>30 ? 5 : 3 );
-	int AVACTLEN = ( LINES>30 ? 8 : 4 );
+	// base it on area instead of width
+	int AVSLIDELEN = ( LINES>30 ? 2 : 1 );
+	int AVALTLET = ( LINES>30 ? 4 : 2 );
 
 	while (true) {
 
-		// doSummat(&lonewriteToPoint);
-
 		int action = rand() % 100;
 
-		if (action < 14) {
+		if (action < 10) {
 
-			// Alter
+			// Alter: add some new symbols and a white "active" symbol (goes too fast atm)
 
 			int x = rand() % COLS;
-			int y = rand() % (int)((double)LINES * 1000.5);
+			int y = rand() % (int)((double)LINES * 4);
 			if (y>=LINES)
 				y=0;
 
 			attrset(COLOR_PAIR(2));
-			while ( (rand() % AVACTLEN) != 0 && y < LINES ) {
+			do {
 
 				move(y,x);
-				// attrset(COLOR_PAIR(7) | A_BOLD);
-				// vaddch(y,x,ch);
+				attrset(COLOR_PAIR(7) | A_BOLD);
 				addch('%');
+
+				wrefresh(stdscr);
 
 				if (y>0) {
 					mbit symbol = palette[ rand() % paletteSize ];
 					move(y-1,x);
-					// attrset(COLOR_PAIR(2));
-					// addch('*');
+					attrset(COLOR_PAIR(2));
 					addch(symbol);
 					thematrix[x][y-1] = symbol;
 
@@ -109,33 +121,38 @@ void main() {
 
 				y++;
 
-			}
+			} while ( (rand() % AVALTLET) != 0 && y < LINES );
 
 			if ( y<LINES && (rand() % 2)==0 ) {
 				move(y,x);
 				attrset(COLOR_PAIR(7) | A_BOLD);
 				addch('%');
+				thematrix[x][y] = '%';
 			}
 
 		} else if (action < 100) {
 
+			// Slide: slide a whole column down n spaces
+
 			// while ( (rand() % 10) != 0 ) {
 
-				// Slide
-				
 				int x = rand() % COLS;
 
 				attrset(COLOR_PAIR(2));
-				while ( (rand() % AVSLIDELEN) != 0 ) {
+				do {
 
 					for (int y=LINES-1;y>0;y--) {
 						mbit src = thematrix[x][y-1];
 						thematrix[x][y] = src;
 						move(y,x);
 						if (src == '%') {
-							attrset(COLOR_PAIR(7) | A_BOLD );
-							addch(thematrix[x][y]);
-							attrset(COLOR_PAIR(2));
+							if ( (rand() % 6*AVSLIDELEN) == 0 ) {
+								thematrix[x][y] = palette[ rand() % paletteSize ];
+							} else {
+								attrset(COLOR_PAIR(7) | A_BOLD );
+								addch(thematrix[x][y]);
+								attrset(COLOR_PAIR(2));
+							}
 						} else {
 							addch(thematrix[x][y]);
 						}
@@ -146,7 +163,7 @@ void main() {
 
 					wrefresh(stdscr);
 
-				}
+				} while ( (rand() % AVSLIDELEN) != 0 );
 
 			// }
 
