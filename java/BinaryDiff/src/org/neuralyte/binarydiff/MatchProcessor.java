487d0ec440ff958f2b786cca4b62389569c18978
joey <joey>
2007-06-06 07:14:17 +0000

    I think these are old files no longer needed in current implementation.

--- java/BinaryDiff/src/org/neuralyte/binarydiff/MatchProcessor.java
+++
@@ -1,6 +0,0 @@
-package org.neuralyte.binarydiff;
-
-/** joey Jun 15, 2006 8:18:33 PM */
-public interface MatchProcessor {
-    void processMatch(Block thisBlock, Block otherBlock);
-}
--- java/BinaryDiff/src/org/neuralyte/binarydiff/PartialSumDB.java
+++
@@ -1,5 +0,0 @@
-package org.neuralyte.binarydiff;
-
-/** joey Jun 22, 2006 3:38:26 AM */
-public class PartialSumDB {
-}
