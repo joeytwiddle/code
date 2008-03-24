8d976fbef0bcc5b10948d767c66c06efdc40a89d
joey <joey>
2008-03-24 11:02:04 +0000

    from laptop

--- code/unrealscript/build.sh
+++ code/unrealscript/build.sh
@@ -1,21 +1,47 @@
-cat System/UnrealTournament_compiling.ini |
-dos2unix |
-grep "^EditPackages=" |
-afterfirst = |
-# pipeboth |
+## DONE: Maybe the .uc.jpp file has not change, but one of the included files HAS changed, hence a reparse IS required although it doesn't look like it.
+##       OK for now, it will reparse the file, if ANY file ending ".jpp" in that folder is newer than it.  This still doesn't resolve #included files from *other* folders.
 
-while read PKGNAME
+TOPDIR="$PWD"
+# find */Classes -maxdepth 2 -name "*.jpp" |
+find */Classes/*.uc.jpp -type f |
+while read JPPFILE
 do
-	if [ ! -f System/$PKGNAME.u ]
-	then jshinfo "$PKGNAME.u is pending..."
-	elif find . -name "$PKGNAME".uc -newer System/$PKGNAME.u | grep . >/dev/null
+	cd "$TOPDIR"
+	cd "`dirname "$JPPFILE"`"
+	JPPFILE="`basename "$JPPFILE"`"
+	TARGETFILE="`echo "$JPPFILE" | sed 's+\.jpp$++'`"
+	# if [ ! -e "$TARGETFILE" ] || ( [ -f "$TARGETFILE" ] && newer "$JPPFILE" "$TARGETFILE" )
+	if [ ! -e "$TARGETFILE" ] || find . -name "*.jpp" -newer "$TARGETFILE" | grep . >/dev/null
 	then
-		jshinfo "$PKGNAME.u needs a rebuild..."
-		# verbosely del System/$PKGNAME.u
-		verbosely mv -f System/$PKGNAME.u System/$PKGNAME.u.last
+		[ "$JPP_MAKE_BACKUPS" ] && [ -f "$TARGETFILE" ] && mv "$TARGETFILE" "$TARGETFILE".`geekdate -fine`
+		# verbosely jpp -- "$JPPFILE" | verbosely dog "$TARGETFILE"
+		echo "jpp: $JPPFILE -> $TARGETFILE" >&2
+		jpp -- "$JPPFILE" > "$TARGETFILE"
 	fi
-done 2>&1 | grep . || jshwarn "No .u files out-of-date or missing."
+done
+cd "$TOPDIR"
 
-cd System
-verbosely wine ucc make ini=UnrealTournament_compiling.ini
+
+
+## Problem: comments out required packages!  also adds some packages (e.g. Screen) which we don't actually want to recompile
+# cat compiling.ini | grep EditPackages= |
+# afterlast = |
+# while read PKG
+# do
+	# if [[ -d "$PKG/Classes" ]]
+	# then
+		# if [[ ! -f "System/$PKG.u" ]] || find "$PKG/Classes" -newer "System/$PKG.u" | grep . >/dev/null
+		# then
+			# echo "Needs rebuild: $PKG"
+			# cat compiling.ini | sed "s+^[; ]*EditPackages=$PKG$+EditPackages=$PKG+" | dog compiling.ini
+		# else
+			# echo "Nothing new: $PKG"
+			# cat compiling.ini | sed "s+^[; ]*EditPackages=$PKG$+; EditPackages=$PKG+" | dog compiling.ini
+		# fi
+	# fi
+# done
+
+
+
+cmd /c make
 
--- code/unrealscript/search.sh
+++ code/unrealscript/search.sh
@@ -1,2 +1,9 @@
-find . -name "*.uc" | grep -v "/CVS/" | xargs higrep "$@" |
+WORD="$1" ; shift
+# find . -name "*.uc" |
+find */Classes Epic -name "*.uc" |
+# xargs grep -i "\<$1\>" |
+# xargs grep -i "$@" |
+xargs grep -i "\<$WORD\>" "$@" |
+highlight "\<$WORD\>" |
+# highlight "\<`caseinsensitiveregex "$WORD"`\>" |
 more
