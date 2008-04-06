d5b613023c2994a750c87ab70634866efab1c132
joey <joey>
2008-04-06 16:21:10 +0000

    joey@hwi:/mnt/big/ut/ut_win

--- code/unrealscript/build.sh
+++ code/unrealscript/build.sh
@@ -1,25 +1,25 @@
 ## DONE: Maybe the .uc.jpp file has not change, but one of the included files HAS changed, hence a reparse IS required although it doesn't look like it.
 ##       OK for now, it will reparse the file, if ANY file ending ".jpp" in that folder is newer than it.  This still doesn't resolve #included files from *other* folders.
 
-TOPDIR="$PWD"
-# find */Classes -maxdepth 2 -name "*.jpp" |
-find */Classes/*.uc.jpp -type f |
-while read JPPFILE
-do
-	cd "$TOPDIR"
-	cd "`dirname "$JPPFILE"`"
-	JPPFILE="`basename "$JPPFILE"`"
-	TARGETFILE="`echo "$JPPFILE" | sed 's+\.jpp$++'`"
-	# if [ ! -e "$TARGETFILE" ] || ( [ -f "$TARGETFILE" ] && newer "$JPPFILE" "$TARGETFILE" )
-	if [ ! -e "$TARGETFILE" ] || find . -name "*.jpp" -newer "$TARGETFILE" | grep . >/dev/null ## ANY new jpp file in this folder.
-	then
-		[ "$JPP_MAKE_BACKUPS" ] && [ -f "$TARGETFILE" ] && mv "$TARGETFILE" "$TARGETFILE".`geekdate -fine`
-		# verbosely jpp -- "$JPPFILE" | verbosely dog "$TARGETFILE"
-		echo "jpp: $JPPFILE -> $TARGETFILE" >&2
-		jpp -- "$JPPFILE" > "$TARGETFILE"
-	fi
-done
-cd "$TOPDIR"
+# TOPDIR="$PWD"
+# # find */Classes -maxdepth 2 -name "*.jpp" |
+# find */Classes/*.uc.jpp -type f |
+# while read JPPFILE
+# do
+	# cd "$TOPDIR"
+	# cd "`dirname "$JPPFILE"`"
+	# JPPFILE="`basename "$JPPFILE"`"
+	# TARGETFILE="`echo "$JPPFILE" | sed 's+\.jpp$++'`"
+	# # if [ ! -e "$TARGETFILE" ] || ( [ -f "$TARGETFILE" ] && newer "$JPPFILE" "$TARGETFILE" )
+	# if [ ! -e "$TARGETFILE" ] || find . -name "*.jpp" -newer "$TARGETFILE" | grep . >/dev/null ## ANY new jpp file in this folder.
+	# then
+		# [ "$JPP_MAKE_BACKUPS" ] && [ -f "$TARGETFILE" ] && mv "$TARGETFILE" "$TARGETFILE".`geekdate -fine`
+		# # verbosely jpp -- "$JPPFILE" | verbosely dog "$TARGETFILE"
+		# echo "jpp: $JPPFILE -> $TARGETFILE" >&2
+		# jpp -- "$JPPFILE" > "$TARGETFILE"
+	# fi
+# done
+# cd "$TOPDIR"
 
 
 
@@ -27,12 +27,14 @@ cd "$TOPDIR"
 
 ## TODO: What's really nasty is when we compile a script, then try to load it as a lib when recompiling other things.  On hwi atm, it's causing ucc to stick on that package.
 
+TOPDIR="$PWD"
 ## Problem: comments out required packages!  also adds some packages (e.g. Screen) which we don't actually want to recompile
 ##    TODO: But it doesn't have to.  As long as it checks that it is really a custom package.
 cat compiling.ini | dos2unix | grep EditPackages= |
 afterlast = |
 while read PKG
 do
+	cd "$TOPDIR"
 	if [[ -d "$PKG/Classes" ]]
 	then
 		if [[ ! -f "System/$PKG.u" ]] || find "$PKG/Classes" -type f -newer "System/$PKG.u" | grep -v "/CVS/" | grep . >/dev/null
@@ -40,6 +42,13 @@ do
 			echo "Needs rebuild: $PKG"
 			cat compiling.ini | dos2unix | sed "s+^[; ]*EditPackages=$PKG$+EditPackages=$PKG+" | dog compiling.ini
 			[[ -f "System/$PKG.u" ]] && verbosely mv "System/$PKG.u" "System/$PKG.u.last"
+			cd "$PKG"/Classes &&
+			find . -maxdepth 1 -type f -name "*.jpp" |
+			while read JPPFILE
+			do
+				TARGETFILE="`echo "$JPPFILE" | sed 's+\.jpp$++'`"
+				verbosely jpp -- "$JPPFILE" > "$TARGETFILE"
+			done
 		else
 			echo "Nothing new: $PKG"
 			cat compiling.ini | dos2unix | sed "s+^[; ]*EditPackages=$PKG$+; EditPackages=$PKG+" | dog compiling.ini
