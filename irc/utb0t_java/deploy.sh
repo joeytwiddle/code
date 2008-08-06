45daf432914c237641fc3c5fd2c6b7e951c284dc
joey <joey>
2008-08-06 01:52:55 +0000

    joey@hwi:/home/joey/joey/code/irc/utb0t_java

--- irc/utb0t_java/deploy.sh
+++ irc/utb0t_java/deploy.sh
@@ -1,3 +1,4 @@
 # tar cz . | ssh joey@neuralyte.org eval "cd ircbot && tar xz"
 # tar cz bin lib *.sh | ssh joey@neuralyte.org eval "cd ircbot && tar xz"
-tar cz bin *.sh DO_NOT_* | ssh joey@neuralyte.org eval "cd ircbot && tar xzv"
+## Sending src why not it's all small
+tar cz src bin *.sh DO_NOT_* | ssh joey@neuralyte.org eval "cd ircbot && tar xzv"
