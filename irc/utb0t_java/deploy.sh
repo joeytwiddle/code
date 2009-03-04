70c21d01e27e36b97897eef86e792becac189503
joey <joey>
2009-03-04 23:47:05 +0000

    joey@hwi -1 +8 2009/03/04 23:46 GMT

--- irc/utb0t_java/deploy.sh
+++ irc/utb0t_java/deploy.sh
@@ -1,4 +1,13 @@
-# tar cz . | ssh joey@neuralyte.org eval "cd ircbot && tar xz"
-# tar cz bin lib *.sh | ssh joey@neuralyte.org eval "cd ircbot && tar xz"
+# LIVE_HOST=joey@neuralyte.org
+LIVE_HOST=joey@77.37.18.99 ## mox's machine
+
+## Make a backup of the existing deploy:
+BACKUP_NUM=` echolines u v w x y z | chooserandomline `
+verbosely ssh "$LIVE_HOST" eval "tar cfz ircbot-backup.$BACKUP_NUM.tgz ircbot"
+
+## Send the locally built copy to the live server:
+# tar cz . | ssh "$LIVE_HOST" eval "cd ircbot && tar xz"
+# tar cz bin lib *.sh | ssh "$LIVE_HOST" eval "cd ircbot && tar xz"
 ## Sending src why not it's all small
-tar cz src bin *.sh DO_NOT_* | ssh joey@neuralyte.org eval "cd ircbot && tar xzv"
+verbosely tar cz src bin *.sh DO_NOT_* | verbosely ssh "$LIVE_HOST" eval "cd ircbot && tar xzv"
+
