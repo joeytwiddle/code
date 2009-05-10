6bec16d1f636ff20cacaba4d4817fc7f119277a6
joey <joey>
2009-05-10 16:48:33 +0000

    *** empty log message ***

--- other/gm_scripts/reclaim_cpu/reclaim_cpu.user.js
+++ other/gm_scripts/reclaim_cpu/reclaim_cpu.user.js
@@ -16,22 +16,34 @@
 // In about:config set image.animation_mode = "once".
 
 //// Config ////
-var secondsBeforeStoppingJavascript = 60;
-// var secondsBeforeStoppingJavascript = 15;
+
+var bool onlyIfIdle = true;
+
+var checkSeconds = 15; // When onlyIfIdle==false, will trigger cleanup after
+							  // exactly this time.  When onlyIfIdle==true, will
+							  // trigger cleanup after at least twice this time.
 
 /*
 
 TODO: Rename to CPU Reclaimer.
 
-TODO: As well as killing JS timers, kill flash apps (remove all object tags I
-guess)!
+DONE: As well as killing JS timers, kill flash apps (remove all object and
+embed tags I guess)!
 
 TODO: Optionally remove all gif's.  (Presuming we can't just stop them.)
 
 TODO: Delay the script disabling until user has stopped using the page.  Detect
-user-idle by checking if his mouse x/y has changed.
+user-idle by checking if his mouse x/y has changed.  Hmm that might trigger
+when not wanted, e.g. when user is just watching a video.  Can we check if the
+user's focus has moved away from the current tab to another tab, or off the
+browser window entirely?
+Maybe we can have a global GM_setvar which can track the last tab(window) to
+detect an event (e.g. mouse movement, but we should make this kbd-only
+compatible too!).
 
-TODO: Rather than removing elements, replace them with "[ELEMENT REMOVED]".
+DONE: Rather than removing elements, replace them with "[ELEMENT REMOVED]".
+TODO: We could offer a link/button which the user can click if they want to
+restore the element.
 
 TODO: Doing this on Google Maps page appears to cause some new transfers to
 take place!  Mmm I think maybe Google Maps is pure Javascript anyway?  If so,
@@ -43,11 +55,53 @@ var report = "";
 
 
 
+/*
+// Detect if the browser is IE or not.
+// If it is not IE, we assume that the browser is NS.
+var IE = document.all?true:false
+
+// If NS -- that is, !IE -- then set up for mouse capture
+if (!IE) document.captureEvents(Event.MOUSEMOVE)
+
+// Set-up to use getMouseXY function onMouseMove
+document.onmousemove = getMouseXY;
+
+// Temporary variables to hold mouse x-y pos.s
+var tempX = 0
+var tempY = 0
+
+// Main function to retrieve mouse x-y pos.s
+
+function getMouseXY(e) {
+  if (IE) { // grab the x-y pos.s if browser is IE
+    tempX = event.clientX + document.body.scrollLeft
+    tempY = event.clientY + document.body.scrollTop
+  } else {  // grab the x-y pos.s if browser is NS
+    tempX = e.pageX
+    tempY = e.pageY
+  }  
+  // catch possible negative values in NS4
+  if (tempX < 0){tempX = 0}
+  if (tempY < 0){tempY = 0}  
+  // show the position values in the form named Show
+  // in the text fields named MouseX and MouseY
+  document.Show.MouseX.value = tempX
+  document.Show.MouseY.value = tempY
+  return true
+}
+*/
+
+
+
 function cleanupCPUHoggers() {
 	clearJavascriptTimers();
 	removeNastyElements();
 
 	window.status = report;
+	if (GM_log) {
+		// GM_log("On \""+document.title+"\": "+report);
+		GM_log("On \""+window.top.document.title+"\": "+report);
+	}
 
 	/*
 	var d = document.createElement('DIV');
@@ -85,16 +139,33 @@ function removeNastyElements() {
 function removeElemsWithTag(tag) {
 	var nasties = document.getElementsByTagName(tag);
 	report += ", "+nasties.length+" "+tag+"s"; // .map("QWERTYUIOPASDFGHJKLZXCVBNM","qwertyuiopasdfghjklzxcvbnm");
-	for (var i=0;i<nasties.length;i++) {
+	for (var i=nasties.length-1;i>=0;i--) {
 		var lastLength = nasties.length;
 		var node = nasties[i];
-		node.parentNode.removeChild(node);
+		var attribs = "";
+		for (var j=0;j<node.attributes.length;j++) {
+			var attr = node.attributes[j];
+			attribs += ' ' + attr.name + '=' + attr.value;
+		}
+		// var text = "<FONT size='+1'><B>[Removed "+tag+" Element]</B></FONT>";
+		var text = "[REMOVED ELEMENT <"+tag+" "+attribs+"/>]";
+		var sis = node.nextSibling;
+		var dad = node.parentNode;
+		dad.removeChild(node);
+		var textNode = document.createTextNode(text);
+		dad.insertBefore(textNode,sis);
+		/*
+		//// No longer needed now we reversed the order of the loop.
 		// DONE TEST: has nasties.length now changed?!
+		nasties = document.getElementsByTagName(tag); // Sometimes this is needed :f
 		if (nasties.length < lastLength) {
 			i--;
 			// GM_log('nasties.length did indeed drop.');
 			// Yup this happens!
 		}
+		*/
+		// nasties = document.getElementsByTagName(tag);
+		// GM_log("After doing "+i+" nasties.length="+nasties.length);
 	}
 }
 
