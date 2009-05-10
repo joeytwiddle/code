eceb649510a04b4cf3d8606e3234fa700624ee6c
joey <joey>
2009-05-10 21:22:18 +0000

    testing for idle detection

--- other/gm_scripts/reclaim_cpu/reclaim_cpu.user.js
+++ other/gm_scripts/reclaim_cpu/reclaim_cpu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Reclaim CPU
 // @namespace      http://userscripts.org/users/89794   (joeytwiddle)
-// @description    Destroys plugins and disables javascript timers on the page, when it has been left idle, so Firefox stops taking CPU when you are not using it.  Useful if you have a slow PC, or you like to open 20 tabs and then do something else without closing them.
+// @description    Destroys plugins and disables javascript timers on the page, when it has been left idle, so Firefox stops taking CPU when you are not using it.  Useful if you have a slow PC, or you like to open 20 tabs and then do something else without closing them.  It will only stop pages which automatically refresh themselves, if you set the checkSeconds config variable to trigger cleanup <I>before</I> the page reloads itself!
 // @include        *
 // @exclude        http://userscripts.org/*
 // ==/UserScript==
@@ -19,13 +19,15 @@
 
 var onlyIfIdle = true;
 
-var checkSeconds = 2; // When onlyIfIdle==false, will trigger cleanup after
+var idleSeconds = 60; // Page will cleanup if it has not been in view for this many seconds.
+
+var checkSeconds = 15; // When onlyIfIdle==false, will trigger cleanup after
 							  // exactly this time.  When onlyIfIdle==true, will
 							  // trigger cleanup after at least twice this time.
 
 /*
 
-TODO: Rename to CPU Reclaimer.
+DONE: Rename to CPU Reclaimer.
 
 DONE: As well as killing JS timers, kill flash apps (remove all object and
 embed tags I guess)!
@@ -45,6 +47,9 @@ DONE: Rather than removing elements, replace them with "[ELEMENT REMOVED]".
 DONE: We could offer a link/button which the user can click if they want to
 restore the element.
 
+TODO: Make the new element match the size of the old element, so the page shape
+does not change.
+
 TODO: Is it possible to give the user a way to restart the javascript timers in
 the same way?
 
@@ -57,42 +62,6 @@ it might not be hogging the CPU anyway.
 var report = "";
 
 
-
-/*
-// Detect if the browser is IE or not.
-// If it is not IE, we assume that the browser is NS.
-var IE = document.all?true:false
-
-// If NS -- that is, !IE -- then set up for mouse capture
-if (!IE) document.captureEvents(Event.MOUSEMOVE)
-
-// Set-up to use getMouseXY function onMouseMove
-document.onmousemove = getMouseXY;
-
-// Temporary variables to hold mouse x-y pos.s
-var tempX = 0
-var tempY = 0
-
-// Main function to retrieve mouse x-y pos.s
-
-function getMouseXY(e) {
-  if (IE) { // grab the x-y pos.s if browser is IE
-    tempX = event.clientX + document.body.scrollLeft
-    tempY = event.clientY + document.body.scrollTop
-  } else {  // grab the x-y pos.s if browser is NS
-    tempX = e.pageX
-    tempY = e.pageY
-  }  
-  // catch possible negative values in NS4
-  if (tempX < 0){tempX = 0}
-  if (tempY < 0){tempY = 0}  
-  // show the position values in the form named Show
-  // in the text fields named MouseX and MouseY
-  document.Show.MouseX.value = tempX
-  document.Show.MouseY.value = tempY
-  return true
-}
-*/
 
 
 
@@ -145,56 +114,112 @@ function removeElemsWithTag(tag) {
 	for (var i=nasties.length-1;i>=0;i--) {
 		var lastLength = nasties.length;
 		var node = nasties[i];
-
-		// var elemHTML = node.outerHTML; // In all browsers except Mozilla :P
-		var attribs = "";
-		for (var j=0;j<node.attributes.length;j++) {
-			var attr = node.attributes[j];
-			attribs += ' ' + attr.name + '="' + attr.value.replace(/"/g,'%22') + '"';
-		}
-		var elemHTML = "<"+tag+""+attribs+"/>";
-
-		var sis = node.nextSibling;
-		var dad = node.parentNode;
-		dad.removeChild(node);
-		var newNode = document.createElement('SPAN');
-		// newNode.appendChild(document.createTextNode("[REMOVED HEAVY ELEMENT "+elemHTML));
-		newNode.appendChild(document.createTextNode("["));
-
-		var restoreElement = function(evt) {
-			// TODO: Would be neater to create correct element type directly, without the SPAN.
-			// TODO BUG: Oh noes I think the value of newNode is the last value it had, not the value it had when we created the function.
-			var restoredNode = document.createElement('SPAN');
-			restoredNode.innerHTML = elemHTML;
-			newNode.parentNode.insertBefore(restoredNode,newNode);
-			newNode.parentNode.removeChild(newNode);
-			window.status = "Restored: "+elemHTML;
-		};
-		var restoreLink = unsafeWindow.document.createElement("A");
-		restoreLink.textContent = "Restore Removed Plugin";
-		restoreLink.onclick = restoreElement;
-		restoreLink.title = elemHTML;
-		restoreLink.href = "javascript:(function(){})();"; // Just to prevent browser changing page.
-		newNode.appendChild(restoreLink);
-
-		newNode.appendChild(document.createTextNode("]"));
-		dad.insertBefore(newNode,sis);
+		// We must call a seperate function, to make a new context for the inner
+		// function which contains unique variables.
+		destroyNasty(node);
 	}
+}
+
+function destroyNasty(node) {
+
+	//// Grab stuff needed for restoring.
+	// var elemHTML = node.outerHTML; // In all browsers except Mozilla :P
+	var attribs = "";
+	for (var j=0;j<node.attributes.length;j++) {
+		var attr = node.attributes[j];
+		attribs += ' ' + attr.name + '="' + attr.value.replace(/"/g,'%22') + '"';
+	}
+	var elemHTML = "<"+node.tagName+""+attribs+"/>";
+	var sis = node.nextSibling;
+
+	//// Main bit: remove the offending element:
+	var dad = node.parentNode;
+	dad.removeChild(node);
+
+	//// Put a Restore link in its place:
+
+	var newNode = document.createElement('SPAN');
+	// newNode.appendChild(document.createTextNode("[REMOVED HEAVY ELEMENT "+elemHTML));
+	newNode.appendChild(document.createTextNode("["));
+
+	var restoreElement = function(evt) {
+		// TODO: We should really create the element type directly, without
+		// creating a new SPAN.
+		var restoredNode = document.createElement('SPAN');
+		restoredNode.innerHTML = elemHTML;
+		newNode.parentNode.insertBefore(restoredNode,newNode);
+		newNode.parentNode.removeChild(newNode);
+		window.status = "Restored: "+elemHTML;
+	};
+	var restoreLink = unsafeWindow.document.createElement("A");
+	restoreLink.textContent = "Restore Removed Plugin";
+	restoreLink.onclick = restoreElement;
+	restoreLink.title = elemHTML;
+	restoreLink.href = "javascript:(function(){})();"; // Just to prevent browser changing page.
+	newNode.appendChild(restoreLink);
+
+	newNode.appendChild(document.createTextNode("]"));
+	dad.insertBefore(newNode,sis);
+
 }
 
 
 //// Not GM compatible: 
 // window.top.document.cleanupCPUHoggers = cleanupCPUHoggers;
-// setTimeout('window.top.document.cleanupCPUHoggers();',checkSeconds*1000);
+// setTimeout('window.top.document.cleanupCPUHoggers();',idleSeconds*1000);
 //// GM double-plus good:
-setTimeout(cleanupCPUHoggers,checkSeconds*1000);
+// setTimeout(cleanupCPUHoggers,idleSeconds*1000);
 
 /*
+// Old style:
+setTimeout(' (function () { var c, tID, iID; tID = setTimeout(function(){}, 0); for (c=1; c<1000 && c<=tID; ++c) { clearTimeout(tID - c); } iID = setInterval(function(){},1000); for (c=0; c<1000 && c<=iID; ++c) { clearInterval(iID - c); } window.status = "Scripts stopped.  ("+tID+","+iID+")"; })(); ',idleSeconds*1000);
 */
+
+
+
+var mouseHasLeft = false;
+var lastSawMouse = new Date().getTime();
+
+var idleInfo = document.createElement('DIV');
+document.body.insertBefore(idleInfo,unsafeWindow.document.body.firstChild);
+
+var watchMouseMove = function(e) {
+	mouseHasLeft = false;
+	lastSawMouse = new Date().getTime();
+	// idleInfo.textContent = "(" + e.pageX + "," + e.pageY + ")";
+	window.status = "(" + e.pageX + "," + e.pageY + ")";
+	idleInfo.textContent = "Mouse is back.";
+};
+// document.body.addEventListener("mousemove", watchMouseMove, false);
+window.addEventListener("mousemove", watchMouseMove, false);
+window.addEventListener("focus", watchMouseMove, false); // Catches when use re-enters window with keyboard.
+
+var watchMouseLeave = function(e) {
+	mouseHasLeft = true;
+	lastSawMouse = new Date().getTime();
+	window.status = "Mouse has left the window.";
+	idleInfo.textContent = "Mouse has left the window.";
+}
+window.addEventListener("mouseout", watchMouseLeave, false);
+
+// TODO TEST: Is catching the window fine-grained enough?  If we switch to other tabs, does original tab still think it is alive?
+// BUG: If I Alt-Tab to get on/off the window, no events are fired.
 
 /*
-// This works:
-setTimeout(' (function () { var c, tID, iID; tID = setTimeout(function(){}, 0); for (c=1; c<1000 && c<=tID; ++c) { clearTimeout(tID - c); } iID = setInterval(function(){},1000); for (c=0; c<1000 && c<=iID; ++c) { clearInterval(iID - c); } window.status = "Scripts stopped.  ("+tID+","+iID+")"; })(); ',checkSeconds*1000);
+document.body.addEventListener("mousemove", function(ev){
+	window.status = "hello";
+}, false);
 */
 
+function checkIdle() {
+	var now = new Date().getTime();
+	var idleTime = (now-lastSawMouse)/1000;
+	idleInfo.textContent = "Last saw mouse "+ idleTime +"s ago.";
+	if (idleTime > idleSeconds) {
+	} else {
+		setTimeout(checkIdle,5000);
+	}
+}
+
+setTimeout(checkIdle,5000);
 
