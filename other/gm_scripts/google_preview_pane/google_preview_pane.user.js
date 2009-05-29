b1e773dd093e7ced2b5a3618d8e388ee2abc4533
joey <joey>
2009-05-29 21:23:57 +0000

    *** empty log message ***

--- other/gm_scripts/google_preview_pane/google_preview_pane.user.js
+++ other/gm_scripts/google_preview_pane/google_preview_pane.user.js
@@ -2,32 +2,41 @@
 // @name           Google Preview Pane
 // @namespace      http://userscripts.org/users/89794
 // @description    Loads Google results in a Preview Pane on mouse hover
-// @include        http://google.*/search?*
-// @include        http://www.google.*/search?*
+// @include        http://google.*/search?*q=*
+// @include        http://www.google.*/search?*q=*
+// @include        http://google.*/webhp?*q=*
+// @include        http://www.google.*/webhp?*q=*
+// @version        0.9.1
 // ==/UserScript==
 
+// Settings:
 var highlightFocusedResult = true;
+var previewWidth = 0.6;
+var pageHeightUsed = 0.7;
 
-var resultsBlock = window.document.getElementById("res");
 
-var table = window.document.createElement("TABLE");
-var tbody = window.document.createElement("TBODY");
-var row = window.document.createElement("TR");
-var leftCell = window.document.createElement("TD");
-var rightCell = window.document.createElement("TD");
 
-leftCell.width = '50%';
-// leftCell.width = (window.innerWidth/2) +'px';
-rightCell.width = '50%';
-// leftCell.height = window.innerHeight * 0.70;
-// rightCell.height = window.innerHeight * 0.75;
-resultsBlock.style.width = (window.innerWidth/2) + 'px';
-resultsBlock.style.height = (window.innerHeight * 0.70) + 'px';
+var resultsWidth = 1.0 - previewWidth;
 
-// leftCell.scrollable = true;
-// rightCell.scrollable = true;
-// leftCell.style.overflow = 'auto';
+var resultsBlock = document.getElementById("res");
+
+var table = document.createElement("TABLE");
+var tbody = document.createElement("TBODY");
+var row = document.createElement("TR");
+var leftCell = document.createElement("TD");
+var rightCell = document.createElement("TD");
+
+leftCell.style.width = (window.innerWidth * resultsWidth) +'px';
+rightCell.style.width = (window.innerWidth * previewWidth) +'px';
+resultsBlock.style.width = (window.innerWidth * resultsWidth) + 'px';
+resultsBlock.style.height = (window.innerHeight * pageHeightUsed) + 'px';
 resultsBlock.style.overflow = 'auto';
+
+var iframe = document.createElement('IFRAME');
+iframe.width = '100%';
+iframe.height = window.innerHeight * 0.70;
+iframe.style.backgroundColor = '#eeeeee';
+rightCell.appendChild(iframe);
 
 tbody.appendChild(row);
 table.appendChild(tbody);
@@ -36,15 +45,6 @@ row.appendChild(rightCell);
 
 resultsBlock.parentNode.insertBefore(table,resultsBlock);
 leftCell.appendChild(resultsBlock);
-
-// leftCell.style.backgroundColor = '#eeeeee';
-
-var iframe = window.document.createElement('IFRAME');
-iframe.width = '100%';
-iframe.height = window.innerHeight * 0.70;
-rightCell.appendChild(iframe);
-
-iframe.style.backgroundColor = '#eeeeee';
 
 (function(){
 
@@ -57,7 +57,7 @@ iframe.style.backgroundColor = '#eeeeee';
 			if (highlightFocusedResult) {
 				if (lastPreview)
 					lastPreview.style.backgroundColor = "";
-				lastHover.style.backgroundColor = "#ffccff";
+				lastHover.style.backgroundColor = "#bbffbb"; // "#ffccff";
 			}
 			iframe.src = lastHover.href;
 			lastPreview = lastHover;
@@ -66,35 +66,29 @@ iframe.style.backgroundColor = '#eeeeee';
 
 	function helloMouse(evt) {
 		var node = evt.target;
-		// window.status = "Over "+node;
-		if (node.tagName=="A" /*&& node.className=="l"*/) {
+		while (node && node.tagName!="A") {
+			node = node.parentNode;
+		}
+		if (node /*&& node.className=="l"*/) {
+			GM_log("Over "+node);
 			lastHover = node;
 			setTimeout(checkFocus,1000);
-			// setTimeout('checkFocus();',1000);
 		}
 	}
 
 	function goodbyeMouse(evt) {
 		var node = evt.target;
-		// window.status = "Out "+node;
-		if (node.tagName=="A" /*&& node.className=="l"*/) {
+		while (node && node.tagName!="A") {
+			node = node.parentNode;
+		}
+		if (node /*&& node.className=="l"*/) {
+			GM_log("Out "+node);
 			lastHover = null;
 		}
 	}
 
-	resultsBlock.addEventListener('mouseover',helloMouse,false);
-	resultsBlock.addEventListener('mouseout',goodbyeMouse,false);
-
-	unsafeWindow.checkFocus = checkFocus;
+	resultsBlock.addEventListener('mouseover',helloMouse,true);
+	resultsBlock.addEventListener('mouseout',goodbyeMouse,true);
 
 })();
-
-/*
-for (var i=0;i<document.links.length;i++) {
-	if (document.links[i].className == "l") {
-		document.links[i].addEventListener('mouseover',helloMouse,false);
-		document.links[i].addEventListener('mouseout',goodbyeMouse,false);
-	}
-}
-*/
 
