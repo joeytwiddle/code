4ac045804623e840f8db6bae4205fd55ccdd9664
joey <joey>
2009-06-06 11:30:32 +0000

    *** empty log message ***

--- other/gm_scripts/google_preview_pane/google_preview_pane.user.js
+++ other/gm_scripts/google_preview_pane/google_preview_pane.user.js
@@ -2,20 +2,20 @@
 // @name           Google Preview Pane
 // @namespace      http://userscripts.org/users/89794
 // @description    Loads Google results in a Preview Pane on mouse hover
-// @include        http://google.*/search?*q=*
 // @include        http://www.google.*/search?*q=*
-// @include        http://google.*/webhp?*q=*
 // @include        http://www.google.*/webhp?*q=*
-// @version        0.9.9.1
+// @include        http://google.*/search?*q=*
+// @include        http://google.*/webhp?*q=*
+// @version        0.9.9.2
 // ==/UserScript==
 
 // Settings:
 
 var fillWholeWindow = true;    // Bring more of the page into the left pane.
-var keepHeaderAbove = true;    // Avoid bringing the top of the page in.
+var keepHeaderAbove = true;   // Avoid bringing the top of the page in.
 var miniLogo = true;           // miniLogo or removeLogo help to reduce the
 var removeLogo = false;        // width and the height of header.
-var reduceWidth = false; // Fix the width of header.
+var reduceWidth = !keepHeaderAbove;  // Shrink the width of the header and footer.
 var previewWidth = 0.7;        // Size of the preview pane.
 var pageHeightUsed = 0.7;      // Will be overridden later if keepHeaderAbove==true.
 var noPanelBorder = false;     // I like the preview pane to have depth.
@@ -40,67 +40,81 @@ window.addEventListener('load',function(){
 
 	var resultsBlock = document.getElementById("res");
 
-	if (fillWholeWindow) {
-		pageHeightUsed = 0.97;
-		resultsBlock = document.createElement("DIV");
+	// Reformatting which might break
+	try {
 
-		// Copy wanted parts of window into our left-pane block:
-		if (keepHeaderAbove) {
-			pageHeightUsed = ( miniLogo||removeLogo ? 0.80 : 0.75 );
-			var curNode = document.getElementById("ssb").nextSibling;
-			while (curNode) {
-				var nextNode = curNode.nextSibling;
-				resultsBlock.appendChild(curNode);
-				curNode = nextNode;
-			}
-		} else {
-			while (document.body.childNodes.length > 0) {
-				resultsBlock.appendChild(document.body.childNodes[0]);
-			}
-		}
+		if (fillWholeWindow) {
 
-		document.body.appendChild(resultsBlock);
+			pageHeightUsed = 0.97;
+			resultsBlock = document.createElement("DIV");
 
-		try {
-			var annoyingLine = document.getElementsByClassName("gbh")[0];
-			annoyingLine.parentNode.removeChild(annoyingLine);
-			annoyingLine = document.getElementsByClassName("gbh")[0];
-			annoyingLine.parentNode.removeChild(annoyingLine);
-		} catch (e) { }
-
-		if (reduceWidth) {
-
-			// If we pulled more stuff than just the results into the left pane,
-			// then we may need to reduce the width of the contents, or we will
-			// get an unpleasant horizontal scrollbar.
-			// Just about everything except the results can become too wide at
-			// some point, so this is quite a fiddle...
-
-			var nobr = document.getElementsByTagName('nobr')[0];
-			nobr.parentNode.innerHTML = nobr.innerHTML;
-			// document.getElementsByTagName('form')[0].getElementsByTagName("input")[1].size = 20;
-			var inputs = document.getElementsByTagName("INPUT");
-			for (var i=0;i<inputs.length;i++) {
-				if (inputs[i].name == 'q')
-					inputs[i].size = 20;
+			// Copy wanted parts of window into our left-pane block:
+			if (keepHeaderAbove) {
+				pageHeightUsed = ( miniLogo||removeLogo ? 0.80 : 0.72 );
+				var curNode = document.getElementById("ssb").nextSibling;
+				while (curNode) {
+					var nextNode = curNode.nextSibling;
+					resultsBlock.appendChild(curNode);
+					curNode = nextNode;
+				}
+			} else {
+				while (document.body.childNodes.length > 0) {
+					resultsBlock.appendChild(document.body.childNodes[0]);
+				}
 			}
 
-			// This avoids the white-space: wrap but kills the blue header background.
-			document.getElementById("ssb").id = 'not_ssb';
-			// OK we restore the blue background:
-			document.getElementById("not_ssb").style.backgroundColor = '#F0F7F9';
-			document.getElementById("not_ssb").style.borderTop = '1px solid #6890DA';
-			// TODO: But vertical alignment of text is still wrong.
-			// document.getElementById("not_ssb").style.verticalAlign = 'middle';
-			var resText = document.getElementById('prs').nextSibling;
-			resText.style.textAlign = 'right';
+			document.body.appendChild(resultsBlock);
 
-			document.getElementsByTagName("bsf").padding = '0px';
+			// I think only really needed if keepHeaderAbove==false.
+			try {
+				var annoyingLine = document.getElementsByClassName("gbh")[0];
+				annoyingLine.parentNode.removeChild(annoyingLine);
+				annoyingLine = document.getElementsByClassName("gbh")[0];
+				annoyingLine.parentNode.removeChild(annoyingLine);
+			} catch (e) { }
 
-			// TODO:
-			// Google gave me a lot of "Searches related to: resize window iframe" at the bottom
-			// which were too wide (5 column table, one with long word "addeventlistener").
-			// Users with very narrow pane may not be able to fit the 10 next page images.
+			if (reduceWidth) {
+
+				// Makes a few pieces of text smaller, but most have custom style font sizes.
+				document.body.style.fontSize='10px';
+
+				// If we pulled more stuff than just the results into the left pane,
+				// then we may need to reduce the width of the contents, or we will
+				// get an unpleasant horizontal scrollbar.
+				// Just about everything except the results can become too wide at
+				// some point, so this is quite a fiddle...
+
+				// The first nobr was a lone child when I wrote this.
+				// We break it.
+				var nobr = document.getElementsByTagName('NOBR')[0];
+				nobr.parentNode.innerHTML = nobr.innerHTML;
+
+				// document.getElementsByTagName('form')[0].getElementsByTagName("input")[1].size = 20;
+				var inputs = document.getElementsByTagName("INPUT");
+				for (var i=0;i<inputs.length;i++) {
+					if (inputs[i].name == 'q')
+						inputs[i].size = 10;
+				}
+
+				// This avoids the white-space: wrap but kills the blue header background.
+				document.getElementById("ssb").id = 'not_ssb';
+				// OK we restore the blue background:
+				document.getElementById("not_ssb").style.backgroundColor = '#F0F7F9';
+				document.getElementById("not_ssb").style.borderTop = '1px solid #6890DA';
+				// TODO: But vertical alignment of text is still wrong.
+				// document.getElementById("not_ssb").style.verticalAlign = 'middle';
+				var resText = document.getElementById('prs').nextSibling;
+				resText.style.textAlign = 'right';
+
+				// TODO:
+				// Google gave me a lot of "Searches related to: resize window iframe" at the bottom
+				// which were too wide (5 column table, one with long word "addeventlistener").
+				// Users with very narrow pane may not be able to fit the 10 next page images.
+
+				// Footer
+				document.getElementById("bsf").padding = '0px';
+
+			}
 
 		}
 
@@ -116,15 +130,19 @@ window.addEventListener('load',function(){
 				var newImg = document.createElement("IMG");
 				// newImg.src = "/favicon.ico";
 				newImg.src = "http://www.google.com/intl/en_ALL/images/logo.gif";
-				newImg.width = 276/3;
-				newImg.height = 110/3;
+				newImg.width = 276/5;
+				newImg.height = 110/5;
 				pNode.appendChild(newImg);
+				pNode.style.paddingTop = '8px';
+				pNode.style.paddingRight = '0px';
+				document.getElementById('sft').style.marginTop = '0px';
+				document.getElementById('sft').style.marginBottom = '0px';
+				document.getElementById('sff').style.paddingBottom = '0px';
 			}
 		}
 
-		// If we leave room for vertical scrollbar, we won't need horizontal one. :)
-		document.getElementById("res").style.width = (window.innerWidth * resultsWidth - 48) +'px';
-
+	} catch (e) {
+		GM_log("Error during layout: "+e);
 	}
 
 	var table     = document.createElement("TABLE");
@@ -136,7 +154,9 @@ window.addEventListener('load',function(){
 	leftCell.width = resultsWidth*100+"%";
 	rightCell.width = previewWidth*100+"%";
 
-	resultsBlock.style.width = (window.innerWidth * resultsWidth) + 'px';
+	// If we leave room for vertical scrollbar, we won't need horizontal one. :)
+	document.getElementById("res").style.width = (window.innerWidth * resultsWidth - 48) +'px';
+	// resultsBlock.style.width = (window.innerWidth * resultsWidth) + 'px';
 	resultsBlock.style.height = (window.innerHeight * pageHeightUsed) + 'px';
 	resultsBlock.style.overflow = 'auto';
 
@@ -153,7 +173,7 @@ window.addEventListener('load',function(){
 	row.appendChild(leftCell);
 	row.appendChild(rightCell);
 
-	resultsBlock.parentNode.insertBefore(table,resultsBlock);
+	resultsBlock.parentNode.insertBefore(table,resultsBlock.nextSibling);
 	leftCell.appendChild(resultsBlock);
 
 	// The "Sponsored Links" block gets in the way.
@@ -178,9 +198,11 @@ window.addEventListener('load',function(){
 	}
 
 	function checkFocus() {
-		if (lastHover && lastHover != lastPreview) {
+		if (lastHover && getContainer(lastHover) != lastPreview) {
 			if (lastPreview)
 				highlightNode(lastPreview,'');
+			// We don't really seem to use lastHover much - we could have set it
+			// to container from the start.
 			highlightNode(lastHover,'#eeeeff');
 			// GM_log("Got lastHover = "+lastHover);
 			var container = getContainer(lastHover);
@@ -209,7 +231,7 @@ window.addEventListener('load',function(){
 			if (link) {
 				// If we have found a link, only go up if that link was a main result.
 				// Otherwise the Cache selects the div above it, which looks weird.
-				if (link.className == 'l' && link.onmousedown) {
+				if (link.className == 'l') {
 				} else {
 					return link;
 				}
@@ -243,7 +265,7 @@ window.addEventListener('load',function(){
 		// Should we hover on this?
 		if (container && container!=lastPreview) {
 			// If we were just hovered on something, unhighlight it:
-			if (lastHover && lastHover!=lastPreview)
+			if (lastHover && getContainer(lastHover)!=lastPreview)
 				highlightNode(lastHover,'');
 			// OK start hover on this.  checkFocus() will check if we are still
 			// here in hoverTime ms, and if so activate.
@@ -261,7 +283,7 @@ window.addEventListener('load',function(){
 		// Should we stop hovering on this?
 		if (container && container!=lastPreview) {
 			// Clear highlight:
-			if (lastHover && lastHover!=lastPreview)
+			if (lastHover && getContainer(lastHover)!=lastPreview)
 				highlightNode(lastHover,'');
 			lastHover = null;
 			highlightNode(node,'');
@@ -275,4 +297,11 @@ window.addEventListener('load',function(){
 
 
 },false);
+
+
+
+/* CONSIDER: If we were to arrange things in a two or three framed page, the
+ * user would be able to resize the results/preview panes by dragging their
+ * vertical bar separator.  The SplitPreview bookmarklet demonstrates this.
+ */
 
