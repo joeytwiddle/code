44a39b2afa4384b65bd4a86fede79989a7f4929e
joey <joey>
2009-06-26 09:12:20 +0000

    *** empty log message ***

--- java/tools/parser/ActiveReplacement.java
+++ java/tools/parser/ActiveReplacement.java
@@ -1,6 +1,6 @@
 package tools.parser;
 
-/* All my source code is freely distributable under the GNU public licence.
+/* This source code is freely distributable under the GNU public licence.
    I would be delighted to hear if have made use of this code.
    If you make money with this code, please give me some!
    If you find this code useful, or have any queries, please feel free to
