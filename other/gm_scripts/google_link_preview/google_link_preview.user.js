bf95efa6603ce65f5f36223e70a87f994d818ac5
joey <joey>
2009-08-07 14:05:06 +0000

    lazy-loading iframes

--- other/gm_scripts/google_link_preview/google_link_preview.user.js
+++ other/gm_scripts/google_link_preview/google_link_preview.user.js
@@ -8,13 +8,17 @@
 // @include       http://www.google.com/blogsearch*
 // @include       http://*/*
 // @include       https://*/*
-// @description	  Adds Clusty.com-like magnifiers on web and news search results to preview a link in a frame
+// @description   Adds Clusty.com-like magnifiers on web and news search results to preview a link in a frame
 // ==/UserScript==
 
 // Last updated: 2007-08-03
 
 window.setTimeout(function() {
   var XPath;
+
+  // This script has special support for:some sites
+  // TODO: Rather than have strict URL matching, try all XPaths, and then
+  // choose a set which has an appropriate number?
   if (location.href.match(/google/)) {
     if (location.href.match(/\/blogsearch\?/)) {
       XPath = "//a[starts-with(@id, 'p-') and count(img)=0]";
@@ -31,7 +35,7 @@ window.setTimeout(function() {
   for (var i = 0; i < links.snapshotLength; i++) {
     var link = links.snapshotItem(i);
     if(link.hasAttribute("onmousedown")) link.removeAttribute("onmousedown");
-  
+
     var pLink = link.parentNode.insertBefore(document.createElement("a"),
                                              link.nextSibling)
     pLink.href = link.href;
@@ -40,36 +44,49 @@ window.setTimeout(function() {
     pLink.style.marginRight = "0.2em";
     pLink.style.width = "0.1em";
     pLink.style.height = "0.1em";
-    pLink.addEventListener("click", function(e) {
-      e.preventDefault();
-      var pOpen = "data:image/gif;base64,\
-R0lGODlhDAAMAMIGAKZZWatkYcqfjsyikM2mk9KumfLsyPLsyCH5BAEKAAcALAAAAAAM\
-AAwAAAMneEcRpFCFokqIi8Ly4MWfhB1hFnGgZgkj4wjAMEZDPEO1fB%2F5zg8JADs%3D";
-      var pClosed = "data:image/gif;base64,\
-R0lGODlhDAAMAMIGAMwAAKtkYc2Tk8yikM2mk9KumQAAAAAAACH5BAEKAAcALAAAAAAM\
-AAwAAAMyeEcRpFCFokqIix5xytvHtQHcJZDiKAQnR2gqCU1VizEsKWPtYEM%2F307Bgf\
-gGGMxgkAAAOw%3D%3D";
-      this.firstChild.src = this.firstChild.src == pOpen ? pClosed : pOpen;
-      this.title = this.title == "preview"?"close preview":"preview";
-      this.nextSibling.style.display = this.nextSibling
-                                           .style.display == "none" ? "" : "none";
-      if (!this.nextSibling.hasAttribute("src"))
-        this.nextSibling.src = this.previousSibling.href;
-    }, false);
 
-    var img = pLink.appendChild(document.createElement("img"));
-    img.alt = "preview";
-    img.border = "none";
-    img.align = "absmiddle";
-    img.src = "data:image/gif;base64,\
-R0lGODlhDAAMAMIGAKZZWatkYcqfjsyikM2mk9KumfLsyPLsyCH5BAEKAAcALAAAAAAM\
-AAwAAAMneEcRpFCFokqIi8Ly4MWfhB1hFnGgZgkj4wjAMEZDPEO1fB%2F5zg8JADs%3D";
+    // start closure for iframe and thereby pLink
+    (function(){
 
-    var iframe = link.parentNode.insertBefore(document.createElement("iframe"),
-                                             pLink.nextSibling);
-    iframe.style.display = "none";
-    iframe.width = "90%";
-    iframe.height = "250";
-    iframe.appendChild(document.createTextNode(""));
-  }
+      var iframe = null;
+
+      pLink.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        if (iframe == null) {
+          iframe = link.parentNode.insertBefore(document.createElement("iframe"), pLink.nextSibling);
+          iframe.style.display = "none";
+          iframe.width = "90%";
+          iframe.height = "250";
+          iframe.appendChild(document.createTextNode("")); // Initial empty body
+        }
+
+        var pOpen = "data:image/gif;base64,\
+  R0lGODlhDAAMAMIGAKZZWatkYcqfjsyikM2mk9KumfLsyPLsyCH5BAEKAAcALAAAAAAM\
+  AAwAAAMneEcRpFCFokqIi8Ly4MWfhB1hFnGgZgkj4wjAMEZDPEO1fB%2F5zg8JADs%3D";
+        var pClosed = "data:image/gif;base64,\
+  R0lGODlhDAAMAMIGAMwAAKtkYc2Tk8yikM2mk9KumQAAAAAAACH5BAEKAAcALAAAAAAM\
+  AAwAAAMyeEcRpFCFokqIix5xytvHtQHcJZDiKAQnR2gqCU1VizEsKWPtYEM%2F307Bgf\
+  gGGMxgkAAAOw%3D%3D";
+        this.firstChild.src = this.firstChild.src == pOpen ? pClosed : pOpen;
+        this.title = this.title == "preview"?"close preview":"preview";
+        this.nextSibling.style.display = this.nextSibling
+                                             .style.display == "none" ? "" : "none";
+        if (!this.nextSibling.hasAttribute("src"))
+          this.nextSibling.src = this.previousSibling.href;
+      }, false);
+
+      var img = pLink.appendChild(document.createElement("img"));
+      img.alt = "preview";
+      img.border = "none";
+      img.align = "absmiddle";
+      img.src = "data:image/gif;base64,\
+  R0lGODlhDAAMAMIGAKZZWatkYcqfjsyikM2mk9KumfLsyPLsyCH5BAEKAAcALAAAAAAM\
+  AAwAAAMneEcRpFCFokqIi8Ly4MWfhB1hFnGgZgkj4wjAMEZDPEO1fB%2F5zg8JADs%3D";
+
+    })(); // end closure for iframe, pLink
+
+  } // end for i < links.snapshotLength
+
 });
+
