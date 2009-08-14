14b6f51b6d7d6b87efe87d9b3ceb5f3f163ca797
joey <joey>
2009-08-14 22:49:47 +0000

    *** empty log message ***

--- other/gm_scripts/userscriptsorguserscript/userscriptsorguserscript.user.js
+++ other/gm_scripts/userscriptsorguserscript/userscriptsorguserscript.user.js
@@ -1,43 +1,64 @@
 // ==UserScript==
-// @name           Userscripts.orgUserscript
+// @name           Userscripts.org Userscript
 // @namespace      UOU
+// @author         joeytwiddle
 // @include        http://userscripts.org/*
 // @include        http://*.userscripts.org/*
 // ==/UserScript==
 
+// I don't play Travian or use Orkut.  Get the hell away from me!
+
+// Sometimes the text touches the edge of the browser, which looks naff.
 document.body.style.padding = '6px';
 
-var newSearch = document.createElement('DIV');
-
-/*
-newSearch.innerHTML = '<form id="script_search" method="get" action="http://www.google.com/search">' +
-	'<input id="script_q" type="text" value="Search with Google" name="q"/>' +
-	'' +
-	'' +
-	'' +
-	'';
-
-var submitButton = document.createElement("BUTTON");
-submitButton.textContent = ">>";
-submitButton.addEventListener("click",doSearch,false);
-*/
-
+// Userscripts.org search engine sucks.  Let's use Google.
 function doSearch(evt){
 	evt.preventDefault();
-	var script_q = document.getElementById("script_q").value;
-	if (script_q) {
-		var target = "http://www.google.com/search?q="+encodeURIComponent(script_q+" site:userscripts.org/scripts/show");
+	var value = document.getElementById("script_q").value;
+	var defaultValue = document.getElementById("script_q").defaultValue;
+	if (value) {
+		var target = "http://www.google.com/search?q="+encodeURIComponent(value+" site:userscripts.org/scripts/show");
 		document.location = target;
 	}
 	return false;
 }
+var searchForm = document.getElementById("script_search");
+var searchButton = document.getElementById("search-go");
+searchForm.addEventListener("submit",doSearch,true);
+searchButton.addEventListener("click",doSearch,true);
 
-var oldSearch = document.getElementById("script_search");
-
-// oldSearch.parentNode.replaceChild(oldSearch,newSearch);
-
-oldSearch.addEventListener("click",doSearch,true);
-oldSearch.addEventListener("submit",doSearch,true);
-// oldSearch.addEventListener("blur",doSearch,true);
-document.getElementById("script_q").value = "Search with Google";
+// Use color strength in the tag cloud:
+var tagCloud = unsafeWindow.document.getElementById("tag-cloud");
+if (tagCloud) {
+	var puffs = tagCloud.getElementsByTagName("LI");
+	GM_log("puffs.length = "+puffs.length);
+	for (var i=0; i<puffs.length; i++) {
+		var puff = puffs[i];
+		if (!puff.style)
+			continue;
+		var fontSize = parseFloat(puff.style.fontSize,10);
+		var strength = 1.7*(fontSize-0.7);
+		if (strength>1.0) strength=1.0;
+		if (strength<0.0) strength=0.0;
+		var weakness = 1.0 - strength;
+		// if (Math.random() < 0.03)
+			// GM_log("puff="+puff+" firstChild="+puff.firstChild);
+		var anchor = puff.getElementsByTagName("A")[0];
+		if (anchor)
+			puff = anchor;
+		else
+			continue;
+		var base = 10;
+		var red = parseInt( base*strength + 255*weakness );
+		var green = parseInt( 100*strength + 255*weakness );
+		var blue = parseInt( 190*strength + 255*weakness );
+		var antisaturation = 255 - strength;
+		puff.style.color = "rgb("+red+","+green+","+blue+")";
+		// if (Math.random() < 0.03)
+			// GM_log(""+puff+".style.color = "+puff.style.color);
+	}
+}
+// It doesn't work so well on the front-page, since there are only two sizes of
+// font atm, and they are specified in 'em' unlike the main tag cloud page
+// which uses 'px'.
 
