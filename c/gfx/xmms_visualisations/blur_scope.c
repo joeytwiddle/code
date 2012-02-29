6191eb668f00bf0e29182dc8a6956363a32c23ac
joey <joey>
2012-02-29 02:30:14 +0000

    blur_scope.c.joey10g

--- c/gfx/xmms_visualisations/blur_scope.c
+++ c/gfx/xmms_visualisations/blur_scope.c
@@ -122,8 +122,12 @@ void bscope_read_config(void)
 }
 
 
-#define blurTao 0.94
-#define fadeRate 0.97
+// #define blurTao 0.9961
+// #define fadeRate 0.9961
+// #define blurTao 0.94
+// #define fadeRate 0.97
+#define blurTao 0.9961
+#define fadeRate 0.94
 
 // #ifndef I386_ASSEM
 void bscope_blur_8_no_asm(guchar *srcptr, guchar *ptr,gint w, gint h, gint bpl)
