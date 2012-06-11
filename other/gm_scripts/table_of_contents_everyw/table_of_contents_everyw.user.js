7dcd584a1502d585a92abec61052e087a825c597
joey <joey>
2012-06-11 02:10:28 +0000

    a bunch of horrid changes to make it work for konqueror

--- other/gm_scripts/table_of_contents_everyw/table_of_contents_everyw.user.js
+++ other/gm_scripts/table_of_contents_everyw/table_of_contents_everyw.user.js
@@ -24,6 +24,8 @@ var pushAnchorsToBottom = true;   // They can look messy interspersed amongst TO
 
 setTimeout(function(){
 
+
+
 // Implementing these two means we can run as a stand-alone script on any page.
 if (typeof GM_log == "undefined") {
 	GM_log = function() {
@@ -78,6 +80,45 @@ if (typeof GM_setValue == 'undefined' || window.navigator.vendor.match(/Google/)
 
 }
 
+function loadScript(url,thenCallFn) {
+	GM_log("[TOCE] Loading fallback: "+url);
+	var scr = document.createElement("script");
+	scr.src = url;
+	scr.type = "text/javascript";   // Konqueror 3.5 needs this!
+	if (thenCallFn) {
+		var called = false;
+		function onceOnlyCallback(evt) {
+			if (!called) {
+				called = true;
+				thenCallFn(evt);
+			}
+		}
+		function errorCallback(evt) {
+			GM_log("[TOCE] Failed to load: "+url,evt);
+			onceOnlyCallback(evt);
+		}
+		scr.addEventListener('load',onceOnlyCallback,false);
+		scr.addEventListener('error',errorCallback,false);
+		// Fallback in case above events unsupported by browser (e.g. Konq 3.5)
+		setTimeout(onceOnlyCallback,5000);
+	}
+	document.body.appendChild(scr);
+}
+
+function newNode(tag,data) {
+	var elem = document.createElement(tag);
+	if (data) {
+		for (var prop in data) {
+			elem[prop] = data[prop];
+		}
+	}
+	return elem;
+}
+
+function newSpan(text) {
+	return newNode("span",{textContent:text});
+}
+
 // Modified for this script's needs.
 // Returns e.g. "/*[2]/*[4]/*[9]"
 function getXPath(node) {
@@ -103,198 +144,195 @@ function getXPath(node) {
 	return getXPath(parent) + '/*' + '[' + thisCount + ']';
 }
 
-try {
-
-	function newNode(tag,data) {
-		var elem = document.createElement(tag);
-		if (data) {
-			for (var prop in data) {
-				elem[prop] = data[prop];
-			}
+// Konqueror 3.5 lacks some things!
+if (!Array.prototype.map) {
+	Array.prototype.map = function(fn) {
+		var l = [];
+		for (var i=0;i<this.length;i++) {
+			l.push(fn(this[i]));
 		}
-		return elem;
-	}
+		return l;
+	};
+}
+if (!String.prototype.trim) {
+	String.prototype.trim = function() {
+		return this.replace(/^[ \t]+/,'').replace(/[ \t]+$/,'');
+	};
+}
 
-	function newSpan(text) {
-		return newNode("span",{textContent:text});
-	}
 
-	var tocFound = document.getElementById("toc");
-	// Konqueror 3.5 does NOT have document.getElementsByClassName(), so we check for it.
-	tocFound = tocFound || (document.getElementsByClassName && document.getElementsByClassName("toc")[0]);
-	tocFound = tocFound || document.getElementById("article-nav");   // developer.mozilla.org
-	tocFound = tocFound || document.getElementById("page-toc");      // developer.mozilla.org
 
-	var toc = tocFound;
+// == Main == //
 
-	// With the obvious exception of Wikimedia sites, most found tocs do not contain a hide/close button.
-	// TODO: If we are going to make the toc float, we should give it rollup/close buttons, unless it already has them.
+function buildTableOfContents() {
 
-	if (!toc) {
+	// Can we make a TOC?
+	var headers = "//h1 | //h2 | //h3 | //h4 | //h5 | //h6 | //h7 | //h8";
+	var anchors = "//a[@name]";
+	// For coffeescript.org:
+	var elementsMarkedAsHeader = "//*[@class='header']";
+	var nodeSnapshot = document.evaluate(headers+(showAnchors?"|"+anchors:"")+"|"+elementsMarkedAsHeader,document,null,6,null);
+	//// Chrome needs lower-case 'h', Firefox needs upper-case 'H'!
+	// var nodeSnapshot = document.evaluate("//*[starts-with(name(.),'h') and substring(name(.),2) = string(number(substring(name(.),2)))]",document,null,6,null);
+	// var nodeSnapshot = document.evaluate("//*[starts-with(name(.),'H') and substring(name(.),2) = string(number(substring(name(.),2)))]",document,null,6,null);
+	if (nodeSnapshot.snapshotLength > minimumItems) {
 
-		// Can we make a TOC?
-		var headers = "//h1 | //h2 | //h3 | //h4 | //h5 | //h6 | //h7 | //h8";
-		var anchors = "//a[@name]";
-		// For coffeescript.org:
-		var elementsMarkedAsHeader = "//*[@class='header']";
-		var nodeSnapshot = document.evaluate(headers+(showAnchors?"|"+anchors:"")+"|"+elementsMarkedAsHeader,document,null,6,null);
-		//// Chrome needs lower-case 'h', Firefox needs upper-case 'H'!
-		// var nodeSnapshot = document.evaluate("//*[starts-with(name(.),'h') and substring(name(.),2) = string(number(substring(name(.),2)))]",document,null,6,null);
-		// var nodeSnapshot = document.evaluate("//*[starts-with(name(.),'H') and substring(name(.),2) = string(number(substring(name(.),2)))]",document,null,6,null);
-		if (nodeSnapshot.snapshotLength > minimumItems) {
+		GM_log("[TOCE] Making TOC with "+nodeSnapshot.snapshotLength+" nodes.");
 
-			GM_log("[TOCE] Making TOC with "+nodeSnapshot.snapshotLength+" nodes.");
+		var toc = newNode("div");
+		toc.id = 'toc';
 
-			toc = newNode("div");
-			toc.id = 'toc';
+		var heading = newSpan("Table of Contents");
+		heading.style.fontWeight = "bold";
+		heading.style.fontSize = "100%";
+		toc.appendChild(heading);
 
-			var heading = newSpan("Table of Contents");
-			heading.style.fontWeight = "bold";
-			heading.style.fontSize = "100%";
-			toc.appendChild(heading);
+		var closeButton = newSpan("[X]");
+		closeButton.style.float = 'right';
+		closeButton.style.cursor = 'pointer';
+		closeButton.style.paddingLeft = '5px';
+		closeButton.onclick = function() { toc.parentNode.removeChild(toc); };
+		toc.appendChild(closeButton);
 
-			var closeButton = newSpan("[X]");
-			closeButton.style.float = 'right';
-			closeButton.style.cursor = 'pointer';
-			closeButton.style.paddingLeft = '5px';
-			closeButton.onclick = function() { toc.parentNode.removeChild(toc); };
-			toc.appendChild(closeButton);
+		var table = newNode("div");
 
-			var table = newNode("div");
-
-			var rollupButton = newSpan("[-]");
-			rollupButton.style.float = 'right';
-			rollupButton.style.cursor = 'pointer';
-			rollupButton.style.paddingLeft = '10px';
-			function toggleRollUp() {
-				if (table.style.display == 'none') {
-					table.style.display = '';
-					rollupButton.textContent = "[-]";
-				} else {
-					table.style.display = 'none';
-					rollupButton.textContent = "[+]";
-				}
-				setTimeout(function(){
-					GM_setValue("TOCE_rolledUp", table.style.display=='none');
-				},5);
+		var rollupButton = newSpan("[-]");
+		rollupButton.style.float = 'right';
+		rollupButton.style.cursor = 'pointer';
+		rollupButton.style.paddingLeft = '10px';
+		function toggleRollUp() {
+			if (table.style.display == 'none') {
+				table.style.display = '';
+				rollupButton.textContent = "[-]";
+			} else {
+				table.style.display = 'none';
+				rollupButton.textContent = "[+]";
 			}
-			rollupButton.onclick = toggleRollUp;
-			toc.appendChild(rollupButton);
-			if (GM_getValue("TOCE_rolledUp",false)) {
-				toggleRollUp();
-			}
-
-			// The xpath query did not return the elements in page-order.
-			// We sort them back into the order they appear in the document
-			// Yep it's goofy code, but it works.
-			var nodeArray = [];
-			for (var i=0;i<nodeSnapshot.snapshotLength;i++) {
-				var node = nodeSnapshot.snapshotItem(i);
-				nodeArray.push(node);
-				// We need to sort numerically, since with strings "24" < "4"
-				node.magicPath = getXPath(node).substring(3).slice(0,-1).split("]/*[").map(Number);
-				if (pushAnchorsToBottom && node.tagName==="A") {
-					node.magicPath.unshift(+Infinity);
-				}
-			}
-			nodeArray.sort(function(a,b){
-				// GM_log("Comparing "+a.magicPath+" against "+b.magicPath);
-				for (var i=0;i<a.magicPath.length;i++) {
-					if (i >= b.magicPath.length) {
-						return +1; // b wins (comes earlier)
-					}
-					if (a.magicPath[i] > b.magicPath[i]) {
-						return +1; // b wins
-					}
-					if (a.magicPath[i] < b.magicPath[i]) {
-						return -1; // a wins
-					}
-				}
-				return -1; // assume b is longer, or they are equal
-			});
-
-			for (var i=0;i<nodeArray.length;i++) {
-				var node = nodeArray[i];
-
-				var level = (node.tagName.substring(1) | 0) - 1;
-				if (level < 0) {
-					level = 0;
-				}
-
-				var linkText = node.textContent && node.textContent.trim() || node.name;
-				if (!linkText) {
-					continue;   // skip things we cannot name
-				}
-
-				var link = newNode("A");
-				if (linkText.length > 40) {
-					link.title = linkText;   // Show full title on hover
-					linkText = linkText.substring(0,32)+"...";
-				}
-				link.textContent = linkText;
-				/* Dirty hack for Wikimedia: */
-				if (link.textContent.substring(0,7) == "[edit] ") {
-					link.textContent = link.textContent.substring(7);
-				}
-				if (node.tagName == "A") {
-					link.href = '#'+node.name;
-				} else {
-					(function(node){
-						link.onclick = function(evt){
-							node.scrollIntoView();
-
-							// Optional: CSS animation
-							// NOT WORKING!
-							/*
-							node.id = "toc_current_hilight";
-							["","-moz-","-webkit-"].forEach(function(insMode){
-								GM_addStyle("#toc_current_hilight { "+insMode+"animation: 'fadeHighlight 4s ease-in 1s alternate infinite'; }@"+insMode+"keyframes fadeHighlight { 0%: { background-color: yellow; } 100% { background-color: rgba(255,255,0,0); } }");
-							});
-							*/
-
-							evt.preventDefault();
-							return false;
-						};
-					})(node);
-					link.href = '#';
-				}
-				table.appendChild(link);
-
-				// For better layout, we will now replace that link with a neater li.
-				liType = "li";
-				if (node.tagName == "A") {
-					liType = "div";
-				}
-				var li = newNode(liType);
-				link.parentNode.replaceChild(li,link);
-				if (node.tagName == "A") {
-					li.appendChild(document.createTextNode("\u2693 "));
-				}
-				li.appendChild(link);
-				li.style.paddingLeft = (1.5*level)+"em";
-				li.style.fontSize = (100-6*(level+1))+"%";
-				li.style.size = li.style.fontSize;
-
-				// Debugging:
-				/*
-				li.title = node.tagName;
-				if (node.name)
-					li.title += " (#"+node.name+")";
-				li.title = getXPath(node);
-				*/
-
-			}
-			toc.appendChild(table);
-
-			document.body.appendChild(toc);
-
-			// TODO scrollIntoView if newly matching 1.hash exists
-
-		} else {
-			log("Not enough items found to create toc.");
+			setTimeout(function(){
+				GM_setValue("TOCE_rolledUp", table.style.display=='none');
+			},5);
+		}
+		rollupButton.onclick = toggleRollUp;
+		toc.appendChild(rollupButton);
+		if (GM_getValue("TOCE_rolledUp",false)) {
+			toggleRollUp();
 		}
 
+		// The xpath query did not return the elements in page-order.
+		// We sort them back into the order they appear in the document
+		// Yep it's goofy code, but it works.
+		var nodeArray = [];
+		for (var i=0;i<nodeSnapshot.snapshotLength;i++) {
+			var node = nodeSnapshot.snapshotItem(i);
+			nodeArray.push(node);
+			// We need to sort numerically, since with strings "24" < "4"
+			node.magicPath = getXPath(node).substring(3).slice(0,-1).split("]/*[").map(Number);
+			if (pushAnchorsToBottom && node.tagName==="A") {
+				node.magicPath.unshift(+Infinity);
+			}
+		}
+		nodeArray.sort(function(a,b){
+			// GM_log("[TOCE] Comparing "+a.magicPath+" against "+b.magicPath);
+			for (var i=0;i<a.magicPath.length;i++) {
+				if (i >= b.magicPath.length) {
+					return +1; // b wins (comes earlier)
+				}
+				if (a.magicPath[i] > b.magicPath[i]) {
+					return +1; // b wins
+				}
+				if (a.magicPath[i] < b.magicPath[i]) {
+					return -1; // a wins
+				}
+			}
+			return -1; // assume b is longer, or they are equal
+		});
+
+		for (var i=0;i<nodeArray.length;i++) {
+			var node = nodeArray[i];
+
+			var level = (node.tagName.substring(1) | 0) - 1;
+			if (level < 0) {
+				level = 0;
+			}
+
+			var linkText = node.textContent && node.textContent.trim() || node.name;
+			if (!linkText) {
+				continue;   // skip things we cannot name
+			}
+
+			var link = newNode("A");
+			if (linkText.length > 40) {
+				link.title = linkText;   // Show full title on hover
+				linkText = linkText.substring(0,32)+"...";
+			}
+			link.textContent = linkText;
+			/* Dirty hack for Wikimedia: */
+			if (link.textContent.substring(0,7) == "[edit] ") {
+				link.textContent = link.textContent.substring(7);
+			}
+			if (node.tagName == "A") {
+				link.href = '#'+node.name;
+			} else {
+				(function(node){
+					link.onclick = function(evt){
+						node.scrollIntoView();
+
+						// Optional: CSS animation
+						// NOT WORKING!
+						/*
+						node.id = "toc_current_hilight";
+						["","-moz-","-webkit-"].forEach(function(insMode){
+							GM_addStyle("#toc_current_hilight { "+insMode+"animation: 'fadeHighlight 4s ease-in 1s alternate infinite'; }@"+insMode+"keyframes fadeHighlight { 0%: { background-color: yellow; } 100% { background-color: rgba(255,255,0,0); } }");
+						});
+						*/
+
+						evt.preventDefault();
+						return false;
+					};
+				})(node);
+				link.href = '#';
+			}
+			table.appendChild(link);
+
+			// For better layout, we will now replace that link with a neater li.
+			liType = "li";
+			if (node.tagName == "A") {
+				liType = "div";
+			}
+			var li = newNode(liType);
+			link.parentNode.replaceChild(li,link);
+			if (node.tagName == "A") {
+				li.appendChild(document.createTextNode("\u2693 "));
+			}
+			li.appendChild(link);
+			li.style.paddingLeft = (1.5*level)+"em";
+			li.style.fontSize = (100-6*(level+1))+"%";
+			li.style.size = li.style.fontSize;
+
+			// Debugging:
+			/*
+			li.title = node.tagName;
+			if (node.name)
+				li.title += " (#"+node.name+")";
+			li.title = getXPath(node);
+			*/
+
+		}
+		toc.appendChild(table);
+
+		document.body.appendChild(toc);
+
+		// TODO scrollIntoView if newly matching 1.hash exists
+
+		postTOC();
+
+	} else {
+		GM_log("[TOCE] Not enough items found to create toc.");
 	}
+
+}
+
+function postTOC() {
 
 	if (toc) {
 		// We make the TOC float regardless whether we created it or it already existed.
@@ -310,12 +348,48 @@ try {
 			+ "#"+tocID+"       { opacity: 0.2; }"
 			+ "#"+tocID+":hover { opacity: 1.0; }"
 		);
-
 	}
 
-} catch (e) {
-	GM_log("[TOCE] Error! "+e);
 }
+
+function searchForTOC() {
+
+	try {
+
+		var tocFound = document.getElementById("toc");
+		// Konqueror 3.5 does NOT have document.getElementsByClassName(), so we check for it.
+		tocFound = tocFound || (document.getElementsByClassName && document.getElementsByClassName("toc")[0]);
+		tocFound = tocFound || document.getElementById("article-nav");   // developer.mozilla.org
+		tocFound = tocFound || document.getElementById("page-toc");      // developer.mozilla.org
+
+		var toc = tocFound;
+
+		// With the obvious exception of Wikimedia sites, most found tocs do not contain a hide/close button.
+		// TODO: If we are going to make the toc float, we should give it rollup/close buttons, unless it already has them.
+
+		if (toc) {
+
+			postTOC();
+
+		} else {
+
+			buildTableOfContents();
+
+		}
+
+	} catch (e) {
+		GM_log("[TOCE] Error! "+e);
+	}
+
+}
+
+if (document.evaluate /*this.XPathResult*/) {
+	searchForTOC();
+} else {
+	loadScript("http://hwi.ath.cx/javascript/xpath.js", searchForTOC);
+}
+
+
 
 },delayBeforeRunning);
 // We want it to run fairly soon but it can be quite heavy on large pages - big XPath search.
