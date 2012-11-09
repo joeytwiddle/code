a8f4b3f13c60849daffb6ee9394441e24ca0c327
joey <joey>
2012-11-09 14:20:56 +0000

    joey@hwi -1 +2 2012/09/18 22:25 BST

--- code/unrealscript/search.sh
+++ code/unrealscript/search.sh
@@ -1,9 +1,10 @@
 WORD="$1" ; shift
 # find . -name "*.uc" |
 find */Classes Epic/*/Classes Decompiled/*/Classes -name "*.uc" |
+grep -v "/CVS/Base/" |
 # xargs grep -i "\<$1\>" |
 # xargs grep -i "$@" |
 IKNOWIDONTHAVEATTY=1 memo -t "1 hour" xargs grep -i "\<$WORD\>" "$@" |
 # highlight "\<$WORD\>" |
 highlight "\<`caseinsensitiveregex "$WORD"`\>" |
-more
+more -f
