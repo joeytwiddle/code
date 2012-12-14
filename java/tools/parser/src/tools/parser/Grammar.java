e95471374c447892f09336a6f318b712cce8398f
joey <joey>
2012-12-14 10:40:12 +0000

    *** empty log message ***

--- java/tools/parser/src/tools/parser/Grammar.java
+++ java/tools/parser/src/tools/parser/Grammar.java
@@ -1,4 +1,5 @@
 package tools.parser;
+
 import java.lang.String;
 import java.util.HashMap;
 import java.util.List;
@@ -7,12 +8,14 @@ import java.util.Vector;
 import tools.parser.*;
 import tools.parser.extensions.WrapMeaningfulIndentation;
 
-// @todo Replace the Vector with a Hashtable, so that later atom definitions can
+
+
+// @done Replace the Vector with a Hashtable, so that later atom definitions can
 // overwrite earlier ones.
 
 public class Grammar {
-	
-   // public static Vector rulesets=new Vector();
+
+	// public static Vector rulesets=new Vector();
 
 	/*public static HashMap<String, RuleSet> rulesets = new HashMap<String, RuleSet>() {
 		// For backwards-compat, we provide .add().  (This used to be a vector!)
@@ -23,37 +26,43 @@ public class Grammar {
 
 	public HashMap<String, RuleSet> rulesets = new HashMap<String, RuleSet>();
 
-  public RuleSet getrulesetforatom(String type, ParseContext ctx) {
-	  /*
-    for (int i=0;i<rulesets.size();i++) {
-      RuleSet rs=(RuleSet)rulesets.get(i);
-      if (rs.type.equals(type))
-        return rs;
-    }
-    System.out.println("Grammar.getrulesetforatom("+type+"): not found!");
-    System.exit(1);
-    return null;
-    */
-	  return rulesets.get(type);
-  }
-  
-  public void addRuleset(RuleSet ruleset) {
-	  // rulesets.add(ruleset);
-	  rulesets.put(ruleset.type, ruleset);
-  }
+
+	public RuleSet getrulesetforatom(String type, ParseContext ctx) {
+		/*
+		for (int i=0;i<rulesets.size();i++) {
+			RuleSet rs=(RuleSet)rulesets.get(i);
+			if (rs.type.equals(type))
+				return rs;
+		}
+		System.out.println("Grammar.getrulesetforatom("+type+"): not found!");
+		System.exit(1);
+		return null;
+		*/
+		return rulesets.get(type);
+	}
+
+
+	public void addRuleset(RuleSet ruleset) {
+		// rulesets.add(ruleset);
+		rulesets.put(ruleset.type, ruleset);
+	}
+
 
 	public void importNamespace(String namespace, String file) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+
 	public void setOption(String name, String[] args) {
-		
+
 	}
 
 	List<PreProcessor> preProcessors = new Vector<PreProcessor>();
+
+
 	public void addPreProcessor(PreProcessor preProcessor) {
 		preProcessors.add(preProcessor);
-   }
-  
+	}
+
 }
