8b693798ff2d6afe97322ba09759b8f1a4186cfd
Paul Joey Clark <joey@neuralyte.org>
2014-06-22 23:01:30 +0800

    Bugfix: check evt.target not this; added title; added link tooltips; set suitable width; fixed typo.

--- other/gm_scripts/github_notifications_dropdown/github_notifications_dropdown.user.js
+++ other/gm_scripts/github_notifications_dropdown/github_notifications_dropdown.user.js
@@ -14,7 +14,7 @@ function listenForNotificationClick(){
 		notificationButton.css("opacity", "0.3");
 		// Had to use .on and .off here because .one was firing multiple times, dunno why.  O_o
 		notificationButton.off("click", notificationButtonClicked);
-		$.get("/notifications").then(receiveNotificationsPage);
+		$.get("/notifications").then(receiveNotificationsPage).fail(receiveNotificationsPage);
 	}
 }
 
@@ -24,18 +24,25 @@ function receiveNotificationsPage(data, textStatus, jqXHR){
 	var notificationPage = $("<div>").append( $.parseHTML(data) );
 	var notificationsList = notificationPage.find(".notifications-list");
 	var notificationsDropdown = $("<div>").addClass("notifications-dropdown");
-	notificationsDropdown.append(notificationsList);
+	$("<center>Notifications:</center>").appendTo(notificationsDropdown);
 	if (notificationsList.children().length == 0) {
 		notificationsDropdown.append("<center>No new notifications</center>");
 	}
+	// Provide hover text for all links, so if the text is too long to display, it can at least be seen on hover.
+	notificationsList.find("a").each(function(){
+		$(this).attr("title", $(this).text());
+	});
+	notificationsDropdown.append(notificationsList);
 	var seeAll = $("<center><b><a href='/notifications'>Notifications page</a></b></center>");
 	notificationsDropdown.append(seeAll);
+	var width = Math.min(700, window.innerWidth);
 	notificationsDropdown.css({
 		position: "absolute",
 		border: "1px solid #ccc",
 		padding: "2px 6px",
 		"background-color": "#fff",
 		"box-shadow": "0px 3px 8px 0px rgba(0,0,0,0.25)",
+		"width": width+"px",
 	});
 	$("body").append(notificationsDropdown); // Done sooner so we can get its width
 	var top = notificationButton.offset().top + notificationButton.height();
@@ -48,14 +55,14 @@ function receiveNotificationsPage(data, textStatus, jqXHR){
 
 	function listenForCloseNotificationDropdown(){
 		var listeners = $("body, .header a[href='/notifications']");
-		listeners.on("click", considerClosingNoficiationDropdown);
-		function considerClosingNoficiationDropdown(evt){
-			if ($(this).closest(".notifications-dropdown").length){
+		listeners.on("click", considerClosingNotificiationDropdown);
+		function considerClosingNotificiationDropdown(evt){
+			if ($(evt.target).closest(".notifications-dropdown").length){
 				// A click inside the dropdown doesn't count!
 			} else {
 				evt.preventDefault();
 				// We must use .on and .off because .one will fire once per element per event type!
-				listeners.off("click", considerClosingNoficiationDropdown);
+				listeners.off("click", considerClosingNotificiationDropdown);
 				notificationsDropdown.remove();
 				listenForNotificationClick();
 			}
