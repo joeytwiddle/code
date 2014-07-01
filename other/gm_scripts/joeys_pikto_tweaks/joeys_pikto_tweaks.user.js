cb027854184e537cbb4682e35483dd8d91215933
Paul Joey Clark <joey@neuralyte.org>
2014-07-01 14:29:09 +0800

    A smaller change to active tab.

--- other/gm_scripts/joeys_pikto_tweaks/joeys_pikto_tweaks.user.js
+++ other/gm_scripts/joeys_pikto_tweaks/joeys_pikto_tweaks.user.js
@@ -15,11 +15,18 @@ if (typeof GM_addStyle == 'undefined') {
   };
 }
 
-if ( document.location.pathname.match("/$|/themes|/infographics") ) {
-	// Tab-style top-bar for the front pages
-	var bgcol = document.location.pathname.match("/$|/themes") ? '#e3e3e3' : 'white';
-	GM_addStyle(" .top-menu-container ul.menu > li a.active { padding-top: 15px; padding-bottom: 15px; margin-bottom: -2px; border-radius: 10px 10px 0px 0px; -moz-border-radius: 10px 10px 0px 0px; -webkit-border-radius: 10px 10px 0px 0px; -ms-border-radius: 10px 10px 0px 0px; -o-border-radius: 10px 10px 0px 0px; background-color: "+bgcol+"; color: black; } ");
-	// Darker modal
+// On the front pages
+if ( document.location.pathname.match("/$|/themes|/infographics|/gallery") ) {
+	// Active tab is white (or light grey) and rounded
+	//var bgcol = document.location.pathname.match("/$|/themes") ? '#e3e3e3' : 'white';
+	//GM_addStyle(" .top-menu-container ul.menu > li a.active { padding-top: 15px; padding-bottom: 15px; margin-bottom: -2px; border-radius: 10px 10px 0px 0px; -moz-border-radius: 10px 10px 0px 0px; -webkit-border-radius: 10px 10px 0px 0px; -ms-border-radius: 10px 10px 0px 0px; -o-border-radius: 10px 10px 0px 0px; background-color: "+bgcol+"; color: black; } ");
+	// Active tab is a bit taller, and bevelled, with slightly wider borders
+	GM_addStyle(" .top-menu-container ul.menu > li a.active { padding-top: 0.5em; border: 1px solid; border-color: rgba(0,0,0,0.4) rgba(255,255,255,0.1) rgba(255,255,255,0.1) rgba(0,0,0,0.4); } ");
+	GM_addStyle(" .top-menu-container ul.menu > li a.active { -webkit-border-radius: 4px; } ");
+	// Grey rounded tab
+	//GM_addStyle(" .top-menu-container ul.menu > li a.active { padding-bottom: 1.0em; margin-bottom: -0.5em; -webkit-border-radius: 10px 10px 0px 0px; } ");
+	// 1px inset #485456
+	// Darker modal (like in the editor)
 	GM_addStyle(" .modal-backdrop { background: #000000; } ");
 	// My Infographics page ( /infographics )
 	$(".pikto-publish-hover-link *").filter(function(){ return $(this).text()=="open on web"; }).text("View");
@@ -39,9 +46,9 @@ for (var i = messageBoxes.length; i--;) {
 	container.appendChild(messageBox);
 }
 
-// Links to "Saved Piktocharts"
+// Links to "Saved Piktocharts" (topbar on main site, menu in editor).
 $("a").each(function(){
-	if ($(this).text() == "Saved Piktocharts") {
+	if ($(this).text() == "Saved Piktocharts" || $(this).text() == "My Saved Piktocharts") {
 		$(this).text("My Infographics");
 	}
 });
