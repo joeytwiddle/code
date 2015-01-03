8058ff40512d2dfaf432103ae4c82cc6bd80f53b
Paul Joey Clark <joey@neuralyte.org>
2015-01-04 00:20:58 +0800

    Render tabs with a unicode symbol, give it a faint color

--- other/gm_scripts/Tab_size/Tab_size.user.js
+++ other/gm_scripts/Tab_size/Tab_size.user.js
@@ -3,16 +3,40 @@
 // @namespace     http://nv.github.com/tab-size.js/
 // @include       *
 // @description   Replace all tab characters with two spaces (or something else)
-// @version       1.0
+// @version       1.0.2
 // @icon          http://userscripts.ru/js/tab-character-size/chrome/icon_48.png
+// @grant         GM_addStyle
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @grant         GM_registerMenuCommand
 // ==/UserScript==
 
-var REPLACEMENT = '  ';
+// TODO: Make it so tabs display using a unicode symbol, but when selected and copied, copy as normal tabs.
+
+//var REPLACEMENT = '  ';
+//var REPLACEMENT = '->';
+//var REPLACEMENT = '| ';
+var REPLACEMENT = '⤚';
+//var REPLACEMENT = '→';
+//var REPLACEMENT = '⇒';
+//var REPLACEMENT = '⇨';
+//var REPLACEMENT = '➡';
+//var REPLACEMENT = '⟼';     // Wider than 1 char even in monospaced fonts
+//var REPLACEMENT = '↦';
+//var REPLACEMENT = '↠';
+//var REPLACEMENT = '⤇';
+//➩ ➪ ➫ ➬ ➭ ➮ ➯ ➱
+//➟ ➠ 
+
+var style_tabs = true;
+
 if (typeof GM_getValue === 'function') {
 	REPLACEMENT = GM_getValue('tab_replacement') || REPLACEMENT;
+}
+
+if (style_tabs && typeof GM_addStyle === 'function') {
+	GM_addStyle('.tab-size-tab { opacity: 0.2; /* color: rgb(127,127,255); */ }');
+	REPLACEMENT = '<span class="tab-size-tab">' + REPLACEMENT + '</span>';
 }
 
 var pre_elements = document.body.getElementsByTagName('pre');
