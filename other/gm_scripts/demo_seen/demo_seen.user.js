7f8d3d1d26987d3a59d8c5cb53645f303f9206b2
Paul Joey Clark <joey@neuralyte.org>
2015-01-25 03:35:09 +0800

    Added "Demo Seen" userscript

---
+++ other/gm_scripts/demo_seen/demo_seen.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Demo Seen
+// @namespace      DS
+// @homepage       http://userscripts.org/users/89794
+// @description    Adds a "visited" colour to pouet.net, so you can see which pages you have already visited.
+// @include        http://pouet.net/*
+// @include        http://www.pouet.net/*
+// @version        1.0.0
+// @grant          GM_addStyle
+// ==/UserScript==
+
+GM_addStyle("a:visited { color: #7799cc; }");
