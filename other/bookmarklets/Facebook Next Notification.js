b4d2a4c6da87bc0d6e2ecd7b969960a03f9399e6
Paul Joey Clark <joey@neuralyte.org>
2015-07-27 11:58:28 +0100

    Go to notifications page if no notifications were found at the top of the dropdown list.

--- other/bookmarklets/Facebook Next Notification.js
+++ other/bookmarklets/Facebook Next Notification.js
@@ -6,9 +6,13 @@
 	setTimeout(function(){
 		var unreadNotifications = document.querySelectorAll("#fbNotificationsFlyout ul li._4af a");
 		console.log(unreadNotifications.length+" unread notifications");
-		var firstUnread = unreadNotifications[0];
-		var markAsReadButton = firstUnread.parentNode.querySelector("[aria-label='Mark as Read']");
-		markAsReadButton.click();
-		firstUnread.firstChild.click();
+		if (unreadNotifications.length === 0) {
+			document.location = "/notifications";
+		} else {
+			var firstUnread = unreadNotifications[0];
+			var markAsReadButton = firstUnread.parentNode.querySelector("[aria-label='Mark as Read']");
+			markAsReadButton.click();
+			firstUnread.firstChild.click();
+		}
 	},1000);
 }());
