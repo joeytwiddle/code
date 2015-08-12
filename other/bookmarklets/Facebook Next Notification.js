6c161672198b63f727061b18e5b7542579309c48
Paul Joey Clark <joey@neuralyte.org>
2015-08-12 12:51:56 +0100

    Hide all read notifications, forcing Facebook to load older unread notifications.

--- other/bookmarklets/Facebook Next Notification.js
+++ other/bookmarklets/Facebook Next Notification.js
@@ -1,4 +1,11 @@
-(function(){
+javascript:(function(){
+	function addStyle(css) {
+		var node = document.createElement("style");
+		node.type = "text/css";
+		node.innerHTML = css;
+		document.getElementsByTagName("head")[0].appendChild(node);
+	}
+	addStyle("._33c { display: none; } ._33c._4af { display: block; }");
 	var toggleButton = document.querySelector("[data-target=fbNotificationsFlyout]");
 	if (!toggleButton.parentNode.className.match(/\bopenToggler\b/)) {
 		toggleButton.click()
@@ -7,7 +14,7 @@
 		var unreadNotifications = document.querySelectorAll("#fbNotificationsFlyout ul li._4af a");
 		console.log(unreadNotifications.length+" unread notifications");
 		if (unreadNotifications.length === 0) {
-			document.location = "/notifications";
+			/*document.location = "/notifications";*/
 		} else {
 			var firstUnread = unreadNotifications[0];
 			var markAsReadButton = firstUnread.parentNode.querySelector("[aria-label='Mark as Read']");
