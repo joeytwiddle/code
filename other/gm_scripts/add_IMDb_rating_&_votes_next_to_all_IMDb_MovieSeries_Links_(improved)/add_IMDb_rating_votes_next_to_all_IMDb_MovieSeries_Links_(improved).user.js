dfc3dc40fe9ea1c1849f4cff4f82524e22787b49
Paul Joey Clark <joey@neuralyte.org>
2015-11-23 21:04:06 +0800

    Joey's version: mostly improved appearance

--- other/gm_scripts/add_IMDb_rating_&_votes_next_to_all_IMDb_MovieSeries_Links_(improved)/add_IMDb_rating_votes_next_to_all_IMDb_MovieSeries_Links_(improved).user.js
+++ other/gm_scripts/add_IMDb_rating_&_votes_next_to_all_IMDb_MovieSeries_Links_(improved)/add_IMDb_rating_votes_next_to_all_IMDb_MovieSeries_Links_(improved).user.js
@@ -2,7 +2,7 @@
 // @name         add IMDb rating & votes next to all IMDb Movie/Series Links (improved)
 // @description  Adds movie ratings and number of voters to any imdb link. Modified version of http://userscripts.org/scripts/show/96884
 // @author         StackOverflow community (especially Brock Adams)
-// @version        2014-06-06
+// @version        2015-11-23-joeytwiddle
 // @match        *://www.imdb.com/*
 // @grant        GM_xmlhttpRequest
 // @namespace https://greasyfork.org/users/2427
@@ -11,6 +11,7 @@
 
 var maxLinksAtATime     = 100; //-- pages can have 100's of links to fetch. Don't spam server or browser.
 var fetchedLinkCnt      = 0;
+var skipEpisodes        = true; //-- I only want to see ratings for movies or TV shows, not TV episodes
 
 function processIMDB_Links () {
     //--- Get only links that could be to IMBD movie/TV pages.
@@ -26,6 +27,10 @@ function processIMDB_Links () {
             ||  ! /^\/title\/tt\d+\/?$/i.test (currentLink.pathname)
         )
             continue;
+
+        if (skipEpisodes && currentLink.parentNode.className.match(/filmo-episodes/)) {
+            continue;
+        }
 
         if (! currentLink.getAttribute ("data-gm-fetched") ){
             if (fetchedLinkCnt >= maxLinksAtATime){
@@ -87,23 +92,46 @@ function prependIMDB_Rating (resp, targetLink) {
         } else {
            if (ratingM  &&  ratingM.length > 1  &&  votesM  &&  votesM.length > 1) {
             isError     = false;
-            
-           justrate = ratingM[1].substr(0, ratingM[1].indexOf("/"));
+
+               justrate = ratingM[1].substr(0, ratingM[1].indexOf("/"));
+
+               var votes = votesM[1];
+               var votesNum = Number( votes.replace(/,/,'','') );
+               var commas_found = votes.match(/,/,'g');
+               if (commas_found && commas_found.length === 1) {
+                   votes = votes.replace(/,.../, 'k');
+               } else if (commas_found && commas_found.length === 2) {
+                   votes = votes.replace(/,.*,.*/, 'M');
+               }
+
            // ratingTxt   = ratingM[1] + " - " + votesM[1];
-           ratingTxt   = justrate + " - " + votesM[1];
+           ratingTxt   = "<b>" + justrate + "</b>" + " / " + votes;
            colnumber = Math.round(justrate);
            }
         }
     }
 
-    
-    
-    
-    
+
+    targetLink.setAttribute("title", ratingTxt.replace(/<[/]b>/,'','g') );
+
+    if (justrate < 5) {
+        return;
+    }
+
+
+    // Slowly approach full opacity as votesNum increases.  1000 votes results in opacity 0.5.
+    var opacity = 1 - 1 / (1 + 0.001 * votesNum);
+    // Actually let's not start from 0.
+    opacity = 0.1 + 0.9*opacity;
+    // Don't use too many decimal points; it's ugly!
+    //opacity = Math.round(opacity * 10000) / 10000;
+    opacity = opacity.toFixed(3);
+
     color = ["#Faa", "#Faa","#Faa", "#Faa","#Faa", "#F88","#Faa", "#ff7","#7e7", "#5e5", "#0e0", "#ddd"]
     var resltSpan       = document.createElement ("span");
-    resltSpan.innerHTML = '<b><font style="border-radius: 5px;padding: 1px;border: #575757 solid 1px; background-color:' + color[colnumber] + ';">' + ' [' + ratingTxt + '] </font></b>&nbsp;';
+    // resltSpan.innerHTML = '<b><font style="border-radius: 5px;padding: 1px;border: #575757 solid 1px; background-color:' + color[colnumber] + ';">' + ' [' + ratingTxt + '] </font></b>&nbsp;';
     // resltSpan.innerHTML = '<b><font style="background-color:' + justrate + '">' + ' [' + ratingTxt + '] </font></b>&nbsp;';
+    resltSpan.innerHTML = '&nbsp;<font style="font-weight: normal;font-size: 80%;opacity: '+opacity+';border-radius: 3px;padding: 1px 4px;border: #575757 solid 1px; background-color:' + color[colnumber] + ';">' + '' + ratingTxt + '</font>';
 
 
     if (isError)
@@ -112,12 +140,13 @@ function prependIMDB_Rating (resp, targetLink) {
     //var targetLink      = resp.context;
     //console.log ("targetLink: ", targetLink);
 
-    targetLink.parentNode.insertBefore (resltSpan, targetLink);
+    //targetLink.parentNode.insertBefore (resltSpan, targetLink);
+    targetLink.parentNode.insertBefore (resltSpan, targetLink.nextSibling);
 }
 
 //--- Create the continue button
 var continueBttn        = document.createElement ("button");
-continueBttn.innerHTML  = "Get ratings";
+continueBttn.innerHTML  = "Get more ratings";
 
 continueBttn.addEventListener ("click", function (){
         fetchedLinkCnt              = 0;
