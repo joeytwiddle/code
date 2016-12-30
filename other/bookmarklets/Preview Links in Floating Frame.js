cba33075972f3ee655be41290ecf452b0772057e
Paul Joey Clark <joey@neuralyte.org>
2016-12-30 10:28:40 +0800

    Updated to use px and https when needed

--- other/bookmarklets/Preview Links in Floating Frame.js
+++ other/bookmarklets/Preview Links in Floating Frame.js
@@ -1,19 +1,25 @@
 javascript:(function(){
 	var f = document.createElement('iframe');
-	f.style.zIndex = 1000;
-	f.style.position = 'absolute';
-	f.style.width = window.innerWidth * 3/4;
-	f.style.height = window.innerHeight * 3/4;
-	f.style.top = 20;
-	f.style.right = 20;
 	f.id = 'floatingFrame1';
+	f.style.zIndex = 1000;
+	f.style.position = 'fixed';
+	f.style.width = window.innerWidth * 3/4 + 'px';
+	f.style.height = window.innerHeight * 3/4 + 'px';
+	f.style.bottom = 8 + 'px';
+	f.style.right = 8 + 'px';
+	f.style.backgroundColor = '#000088';
 	var links = document.getElementsByTagName('A');
-	for (var i = 0; i < links.length ; i++) {
-		links[i].onmouseover = eventHandler;
-	function eventHandler(evt) {
+	var hoverHandler = function (evt) {
 		var linkNode = evt.currentTarget;
+		var originalLinkProtocol = linkNode.protocol;
+		if (document.location.protocol === 'https:' && originalLinkProtocol === 'http:') {
+			linkNode.protocol = 'https:';
+		}
 		document.getElementById('floatingFrame1').src = linkNode.href;
-	}
+		linkNode.protocol = originalLinkProtocol;
+	};
+	for (var i = 0; i < links.length ; i++) {
+		links[i].onmouseover = hoverHandler;
 	}
 	document.body.appendChild(f);
 }());
