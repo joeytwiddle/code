a63ea6869c51e07c636c1b9d7643e1054c10059b
Paul Joey Clark <joey@neuralyte.org>
2018-01-19 23:42:49 +0800

    Only change font for README; re-arrange

--- other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
+++ other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         npmjs.com visual tweaks
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Styles npmjs.com README pages similarly to GitHub's (font, size, colors, but not syntax highlighting)
+// @version      0.4
+// @description  Makes npmjs.com fonts more like GitHub's
 // @author       joeytwiddle
-// @match        https://www.npmjs.com/package/*
+// @copyright    2018, Paul "Joey" Clark (http://neuralyte.org/~joey)
+// @license      MIT
+// @match        https://www.npmjs.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -14,11 +16,15 @@
     // I want to scale down the fonts and everything else a bit.  This was an easy way to do that.
     GM_addStyle('.container { transform: scale(0.92); transform-origin: 50% 0; }');
 
-    GM_addStyle('body { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Source Sans Pro", "Lucida Grande", sans-serif; }');
-    GM_addStyle('body { font-family: line-height: 1.5;}');
+    // Fill (almost) the whole width of the screen, like Wide-Github does.
+    GM_addStyle('.container { width: 98%; max-width: 100%; }');
 
+    // Set fonts like GitHub
+    GM_addStyle('#readme { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Source Sans Pro", "Lucida Grande", sans-serif; }');
+    GM_addStyle('#readme { font-family: line-height: 1.5;}');
     GM_addStyle('pre, code, kbd, samp { font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace; }');
 
+    // Set font sizes like GitHub
     // A snippet of code appearing within a paragraph
     GM_addStyle('code { font-size: 88%; }');
     // A line of code on its own
@@ -26,19 +32,18 @@
     // A block of code
     GM_addStyle('pre { font-size: 82%; line-height: 1.4; }');
 
+    // Darker text
+    GM_addStyle('.markdown p, .markdown li, code { color: rgb(51, 51, 51); }');
+
+    // Padding around code blocks and snippets
     // A snippet of code appearing within a paragraph
     GM_addStyle('code { padding: 0.2em 0.2em; }');
     // A line of code on its own
     GM_addStyle('pre > code { padding: 1em 2em; }');
 
-    // Darker text
-    GM_addStyle('.markdown p, .markdown li, code { color: rgb(51, 51, 51); }');
+    // Lighter background on code blocks and snippets
+    GM_addStyle('.highlight, code { background-color: #f6f8fa; }');
 
     // More of a gap after each paragraph?  Wasn't actually needed.  The problem was wrapping due to insufficient width.
     //GM_addStyle('div, .highlight { padding-bottom: 20px; }');
-
-    GM_addStyle('.highlight, code { background-color: #f6f8fa; }');
-
-    // Fill (almost) the whole width of the screen, like Wide-Github does.
-    GM_addStyle('.container { width: 98%; max-width: 100%; }');
 })();
