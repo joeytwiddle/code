83602dbf771499f42e49541c8fa4e9dc66b136a4
Paul Joey Clark <joey@neuralyte.org>
2018-01-19 18:13:15 +0800

    Tweaks colors and font sizes

--- other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
+++ other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         npmjs.com visual tweaks
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Makes npmjs.com fonts more like GitHub's
 // @author       joeytwiddle
 // @match        https://www.npmjs.com/package/*
@@ -20,24 +20,26 @@
     GM_addStyle('pre, code, kbd, samp { font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace; }');
 
     // A snippet of code appearing within a paragraph
-    GM_addStyle('code { font-size: 92%; }');
+    GM_addStyle('code { font-size: 88%; }');
     // A line of code on its own
     GM_addStyle('pre > code { font-size: 100%; }');
     // A block of code
     GM_addStyle('pre { font-size: 82%; line-height: 1.4; }');
 
     // A snippet of code appearing within a paragraph
-    GM_addStyle('code { padding: 0.2em 0.5em; }');
+    GM_addStyle('code { padding: 0.2em 0.2em; }');
     // A line of code on its own
     GM_addStyle('pre > code { padding: 1em 2em; }');
 
     // Not black text, but darker than the default #333
-    //GM_addStyle('.markdown p { color: #24292e; }');
+    //GM_addStyle('.markdown p, .markdown li, code { color: #24292e; }');
     // Just black
-    GM_addStyle('.markdown p { color: #000; }');
+    GM_addStyle('.markdown p, .markdown li, code { color: #000; }');
 
     // More of a gap after each paragraph?  Wasn't actually needed.  The problem was wrapping due to insufficient width.
     //GM_addStyle('div, .highlight { padding-bottom: 20px; }');
+
+    GM_addStyle('.highlight, code { background-color: #f6f8fa; }');
 
     // Fill (almost) the whole width of the screen, like Wide-Github does.
     GM_addStyle('.container { width: 98%; max-width: 100%; }');
