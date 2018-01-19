4c6023f7ea606a161709d956dfd9884f133c28a7
Paul Joey Clark <joey@neuralyte.org>
2018-01-19 17:51:54 +0800

    Add npmjs visual tweaks 0.1

---
+++ other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         npmjs.com visual tweaks
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Makes npmjs.com fonts more like GitHub's
+// @author       joeytwiddle
+// @match        https://www.npmjs.com/package/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // I want to scale down the fonts and everything else a bit.  This was an easy way to do that.
+    GM_addStyle('.container { transform: scale(0.92); transform-origin: 50% 0; }');
+
+    GM_addStyle('body { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Source Sans Pro", "Lucida Grande", sans-serif; }');
+    GM_addStyle('body { font-family: line-height: 1.5;}');
+
+    GM_addStyle('pre, code, kbd, samp { font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace; }');
+
+    // A snippet of code appearing within a paragraph
+    GM_addStyle('code { font-size: 92%; }');
+    // A line of code on its own
+    GM_addStyle('pre > code { font-size: 100%; }');
+    // A block of code
+    GM_addStyle('pre { font-size: 82%; line-height: 1.4; }');
+
+    // A snippet of code appearing within a paragraph
+    GM_addStyle('code { padding: 0.2em 0.5em; }');
+    // A line of code on its own
+    GM_addStyle('pre > code { padding: 1em 2em; }');
+
+    // Not black text, but darker than the default #333
+    //GM_addStyle('.markdown p { color: #24292e; }');
+    // Just black
+    GM_addStyle('.markdown p { color: #000; }');
+
+    // More of a gap after each paragraph?  Wasn't actually needed.  The problem was wrapping due to insufficient width.
+    //GM_addStyle('div, .highlight { padding-bottom: 20px; }');
+
+    // Fill (almost) the whole width of the screen, like Wide-Github does.
+    GM_addStyle('.container { width: 98%; max-width: 100%; }');
+})();
