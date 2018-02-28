4d1a4bb6c7d1ba79e07092d0ee846618d90d981c
Paul Joey Clark <joeytwiddle@gmail.com>
2018-02-28 11:53:38 +0800

    Add more sites from the network

--- other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
+++ other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
@@ -2,10 +2,10 @@
 // @name           StackExchange Tweaks
 // @namespace      SET
 // @description    Minor visual tweas to StackExchange
-// @version        1.0.0
-// @include        http://stackoverflow.com/*
+// @version        1.0.1
 // @include        https://stackoverflow.com/*
-// @include        http://*.stackexchange.com/*
+// @include        https://superuser.com/*
+// @include        https://serverfault.com/*
 // @include        https://*.stackexchange.com/*
 // @grant          GM_addStyle
 // ==/UserScript==
