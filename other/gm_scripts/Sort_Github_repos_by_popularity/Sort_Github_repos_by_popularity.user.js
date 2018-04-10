73176d2af208f3ced99054207626472db70c0b8e
Paul Joey Clark <joeytwiddle@gmail.com>
2018-04-10 23:57:01 +0800

    Reformat and tweak a little

--- other/gm_scripts/Sort_Github_repos_by_popularity/Sort_Github_repos_by_popularity.user.js
+++ other/gm_scripts/Sort_Github_repos_by_popularity/Sort_Github_repos_by_popularity.user.js
@@ -2,46 +2,44 @@
 // @name         Sort Github repos by popularity
 // @namespace    http://tampermonkey.net/
 // @license      MIT
-// @version      0.1.1
-// @description  Sort user's Github repositories by popularity
+// @version      0.1.2
+// @description  Sort user's Github repositories by popularity (only applies to those visible on the current page)
 // @author       joeytwiddle
 // @match        https://github.com/*tab=repositories*
 // @grant        none
 // ==/UserScript==
 
 setTimeout(function() {
-    'use strict';
+  'use strict';
 
-    if (document.location.search.split('&').some(x => x === "tab=repositories")) {
-        // Proceed
-    } else {
-        // Wrong page
-        return;
-    }
+  if (document.location.search.split('&').some(x => x === "tab=repositories")) {
+    // Proceed
+  } else {
+    // Wrong page
+    return;
+  }
 
-    const container = document.querySelector('#user-repositories-list > ul');
-    const reposNodeList = document.querySelectorAll('#user-repositories-list > ul > li');
-    const repos = Array.prototype.slice.call(reposNodeList);
+  const container = document.querySelector('#user-repositories-list > ul');
+  const reposNodeList = document.querySelectorAll('#user-repositories-list > ul > li');
+  const repos = Array.prototype.slice.call(reposNodeList);
 
-    //console.log("Repo count:", repos.length);
+  //console.log("Repo count:", repos.length);
 
-    repos.sort((a, b) => {
-        return getPopularity(a) < getPopularity(b) ? +1 : -1;
-    });
+  repos.reverse().forEach(ul => {
+    container.removeChild(ul);
+  });
 
-    repos.forEach(ul => {
-        container.removeChild(ul);
-    });
+  repos.sort((a, b) => getPopularity(a) < getPopularity(b) ? +1 : -1);
 
-    repos.forEach(ul => {
-        container.appendChild(ul);
-    });
+  repos.forEach(ul => {
+    container.appendChild(ul);
+  });
 
-    function getPopularity (li) {
-        const starSvg = li.querySelector('[aria-label=star]');
-        const textElem = starSvg && starSvg.nextSibling;
-        const popularity = textElem && Number(textElem.textContent) || 0;
-        //console.log("Popularity:", popularity);
-        return popularity;
-    }
+  function getPopularity (li) {
+    const starSvg = li.querySelector('[aria-label=star]');
+    const textElem = starSvg && starSvg.nextSibling;
+    const popularity = textElem && Number(textElem.textContent) || 0;
+    //console.log("Popularity:", popularity);
+    return popularity;
+  }
 }, 10);
