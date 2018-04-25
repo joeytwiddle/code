4de3b9fbf31385a6b60cca6299662ee38f14c931
Paul Joey Clark <joeytwiddle@gmail.com>
2018-04-25 22:40:31 +0800

    Remove the growl (text popup)

--- other/gm_scripts/calm_quora_s_annoying_red_notification_dots/calm_quora_s_annoying_red_notification_dots.user.js
+++ other/gm_scripts/calm_quora_s_annoying_red_notification_dots/calm_quora_s_annoying_red_notification_dots.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Calm Quora's annoying red notification dots
 // @namespace    joeytwiddle
-// @version      0.0.3
+// @version      0.0.4
 // @license      MIT
 // @description  The red notifications on Quora are too glaring, appear too frequently, and do not go away easily enough.  Let's make them grey so they aren't such a bother.
 // @author       joeytwiddle
@@ -9,4 +9,8 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
+// Make the red notification dots grey instead
 GM_addStyle('.SiteHeaderBadge, .WriteNavbadge, .red_badge { background: #ddd !important; background-color: #ddd !important; color: #666 !important; transform: scale(0.8); opacity: 0.5; }');
+
+// Don't show any popups
+GM_addStyle('.Growl { display: none !important; }');
