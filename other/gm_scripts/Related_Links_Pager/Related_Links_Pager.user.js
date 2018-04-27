c8a0bbae383c242834549d9e2674ebadc2356ab9
Paul Joey Clark <joeytwiddle@gmail.com>
2018-04-28 01:43:17 +0800

    Add @license meta

--- other/gm_scripts/Related_Links_Pager/Related_Links_Pager.user.js
+++ other/gm_scripts/Related_Links_Pager/Related_Links_Pager.user.js
@@ -3,6 +3,7 @@
 // @namespace      RLP
 // @description    Navigate sideways!  When you click a link, related links on the current page are carried with you.  They can be accessed from a pager on the target page, so you won't have to go back in your browser.
 // @version        1.3.3
+// @license        AGPL-3.0; http://www.gnu.org/licenses/agpl.txt
 // @downstreamURL  http://userscripts.org/scripts/source/124293.user.js
 // @include        http://*/*
 // @include        https://*/*
