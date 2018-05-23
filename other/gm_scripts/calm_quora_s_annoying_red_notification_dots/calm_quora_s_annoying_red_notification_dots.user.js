020264888eb5f40b5d5c0cddbc201aeb1ca9eeeb
Paul Joey Clark <joeytwiddle@gmail.com>
2018-05-23 15:34:49 +0800

    De-emphasise Quora adverts, so they aren't confused with answers

--- other/gm_scripts/calm_quora_s_annoying_red_notification_dots/calm_quora_s_annoying_red_notification_dots.user.js
+++ other/gm_scripts/calm_quora_s_annoying_red_notification_dots/calm_quora_s_annoying_red_notification_dots.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Calm Quora's annoying red notification dots
 // @namespace    joeytwiddle
-// @version      1.0.0
+// @version      1.0.1
 // @license      MIT
-// @description  The red notifications on Quora are too glaring, appear too frequently, and do not go away easily enough.  Let's make them grey so they aren't such a bother.
+// @description  The red notifications on Quora are too glaring, appear too frequently, and do not go away easily enough.  Let's make them grey so they aren't such a bother.  Also the popups and the adverts can take a hike.
 // @author       joeytwiddle
 // @match        https://www.quora.com/*
 // @grant        GM_addStyle
@@ -19,3 +19,15 @@ GM_addStyle('.Growl { display: none !important; }');
 // As requested by Keeni: https://greasyfork.org/en/forum/discussion/37380/x
 // In fact I think I will just apply this to anything I find on the site which is red!
 GM_addStyle('.SiteHeader, .questions_to_answer_icon { filter: saturation(0%); }');
+
+// Quora's advertisements appear visually a lot like the answers, so it is easy to read an advert and get confused because it's talking about a completely tangential topic.
+// For some reason, I decided not to remove the adverts completely, but to de-emphasise them and differentiate them.
+// The padding is needed so that the text doesn't appear to close to the edge of the red box.
+// A hint of Quora red:
+//const cssForAdverts = { backgroundColor: 'hsl(3, 25%, 90%)', opacity: 0.3, padding: '1em' };
+// No background, just de-emphasise.  The padding is not strictly necessary but it can help to differentiate.
+//const cssForAdverts = { opacity: 0.3, padding: '1em' };
+const cssForAdverts = { opacity: 0.1, padding: '0em' };
+jQuery('.advertiser_endpoint').closest('.outer_content_box').css(cssForAdverts)
+
+// Curiously, window.jQuery didn't work in TamperMonkey for Chrome.  unsafeWindow.jQuery did, but I don't want the extra @grant
