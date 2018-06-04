07639d02fc3aab87bc384f8ed0d680515f773957
Paul Joey Clark <joeytwiddle@gmail.com>
2018-06-04 18:57:29 +0800

    Update userscript for new site CSS

--- other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
+++ other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
@@ -2,14 +2,24 @@
 // @name           StackExchange Tweaks
 // @namespace      SET
 // @description    Minor visual tweas to StackExchange
-// @version        1.0.1
+// @version        1.0.2
 // @include        https://stackoverflow.com/*
 // @include        https://superuser.com/*
 // @include        https://serverfault.com/*
 // @include        https://*.stackexchange.com/*
-// @grant          GM_addStyle
+// @grant          none
 // ==/UserScript==
 
 // Swap the positions of the notifications block and the username/stats block
-var secondaryNav = document.querySelector('.secondary-nav')
-secondaryNav.parentNode.insertBefore(secondaryNav, secondaryNav.parentNode.firstChild)
+//var secondaryNav = document.querySelector('.secondary-nav')
+//secondaryNav.parentNode.insertBefore(secondaryNav, secondaryNav.parentNode.firstChild)
+
+(function() {
+    var profileElementInner = document.querySelector('.my-profile');
+    if (!profileElementInner) {
+        console.warn("Could not find .my-profile element");
+        return;
+    }
+    var profileElementContainer = profileElementInner.parentNode;
+    profileElementContainer.parentNode.appendChild(profileElementContainer);
+}());
