7c4e5fcc5dc998bc31c15d56a1bfcf40f938a398
Paul Joey Clark <joeytwiddle@gmail.com>
2018-06-16 13:52:13 +0800

    Hide the new left sidebar

--- other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
+++ other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
@@ -2,24 +2,26 @@
 // @name           StackExchange Tweaks
 // @namespace      SET
 // @description    Minor visual tweas to StackExchange
-// @version        1.0.2
+// @version        1.0.3
 // @include        https://stackoverflow.com/*
 // @include        https://superuser.com/*
 // @include        https://serverfault.com/*
 // @include        https://*.stackexchange.com/*
-// @grant          none
+// @grant          GM_addStyle
 // ==/UserScript==
 
 // Swap the positions of the notifications block and the username/stats block
 //var secondaryNav = document.querySelector('.secondary-nav')
 //secondaryNav.parentNode.insertBefore(secondaryNav, secondaryNav.parentNode.firstChild)
-
-(function() {
-    var profileElementInner = document.querySelector('.my-profile');
-    if (!profileElementInner) {
-        console.warn("Could not find .my-profile element");
-        return;
-    }
+var profileElementInner = document.querySelector('.my-profile');
+if (profileElementInner) {
     var profileElementContainer = profileElementInner.parentNode;
     profileElementContainer.parentNode.appendChild(profileElementContainer);
-}());
+} else {
+    console.warn("Could not find .my-profile element");
+}
+
+// Hide the sidebar when we are on question pages (to reduce visual noise)
+if (document.location.pathname.match(/^\/(q|questions)\//)) {
+    GM_addStyle('#left-sidebar { display: none; }');
+}
