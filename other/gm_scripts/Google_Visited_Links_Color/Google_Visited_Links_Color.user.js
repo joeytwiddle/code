a91e8204e9ed23c759004bba1b87e0ce7cd3d2c2
Paul Joey Clark <joeytwiddle@gmail.com>
2018-06-21 12:17:56 +0800

    Make Google links closer to traditional web colours

--- other/gm_scripts/Google_Visited_Links_Color/Google_Visited_Links_Color.user.js
+++ other/gm_scripts/Google_Visited_Links_Color/Google_Visited_Links_Color.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Visited Links Color
 // @namespace    GVLC
-// @version      0.1
+// @version      0.1.1
 // @description  Make it easier to see which links you have already visited on google search results
 // @author       joeytwiddle
 // @license      MIT
@@ -9,15 +9,28 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
+// The year is 2018.  Google search results have made their links dark blue (#1a0dab) and their visited links dark pink (#609).
+// It it somewhat difficult to tell them apart.  This userscript attempts to make the colours easier to distinguish.
+
 (function() {
     'use strict';
-    // const visitedLinkColor = '#9a8bb1';
-    // const visitedLinkColor = '#9e5fbd';
-    // const visitedLinkColor = '#8c59a6';
-    // const visitedLinkColor = '#946ba8';
-    // const visitedLinkColor = '#986aaf';
-    // The normal blue, heavily grey-ified
-    const visitedLinkColor = '#75738c';
-    GM_addStyle('.mblink:visited, a:visited { color: ' + visitedLinkColor + '; }');
-    // Your code here...
+    //const visitedLinkColor = '#9a8bb1';
+    //const visitedLinkColor = '#9e5fbd';
+    //const visitedLinkColor = '#8c59a6';
+    //const visitedLinkColor = '#946ba8';
+    // A nice faint pink.  Works well (clear separation) but looks a bit odd.
+    //const visitedLinkColor = '#986aaf';
+    //GM_addStyle('.mblink:visited, a:visited { color: ' + visitedLinkColor + '; }');
+
+    // Back to default web colours, applying "least surprise" design principle
+    // Default web colours are (I think): #00C (link) and #609 (visited link)
+    // Here we are using slightly adjusted versions
+    GM_addStyle(`
+      a:link, .w, #prs a:visited, #prs a:active, .q:active, .q:visited, .kl:active, .tbotu {
+        color: #0000aa;
+      }
+      .mblink:visited, a:visited {
+        color: #880099;
+      }
+    `);
 })();
