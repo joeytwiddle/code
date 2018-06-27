1ef854c2674d934be51463d7ab14a80a33a3f672
Paul Joey Clark <joeytwiddle@gmail.com>
2018-06-27 14:13:09 +0800

    Remove the visiting-link highlight after a small delay

--- other/gm_scripts/Related_Links_Pager/Related_Links_Pager.user.js
+++ other/gm_scripts/Related_Links_Pager/Related_Links_Pager.user.js
@@ -2,7 +2,7 @@
 // @name           Related Links Pager
 // @namespace      RLP
 // @description    Navigate sideways!  When you click a link, related links on the current page are carried with you.  They can be accessed from a pager on the target page, so you won't have to go back in your browser.
-// @version        1.3.22
+// @version        1.3.23
 // @license        AGPL-3.0; http://www.gnu.org/licenses/agpl.txt
 // @downstreamURL  http://userscripts.org/scripts/source/124293.user.js
 // @include        http://*/*
@@ -564,6 +564,18 @@ function runRelatedLinksPager() {
       if (highlightLinkGroups) {
         clearList('RLP-visiting-link');
         listOfHighlightedNodes = linksInGroup;
+        // But if this is a SPA, we don't want to leave those highlights around forever
+        // TODO: We could also clear this highlight immediately if we detect a pushState / URL change
+        (function () {
+          var visitedLinksToClear = listOfHighlightedNodes.slice(0);
+          setTimeout(function(){
+              for (var i = 0; i < visitedLinksToClear.length; i++) {
+                  visitedLinksToClear[i].classList.remove('RLP-visiting-link');
+              }
+              // GC
+              visitedLinksToClear = undefined;
+          }, 5000);
+        })();
       }
 
       // On a single page app there won't be a fresh a page load to trigger rebuilding the pager
