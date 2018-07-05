24e33e80a8d9f99952cb98f1727e5204ef1ec8d9
Paul Joey Clark <joeytwiddle@gmail.com>
2018-07-05 13:13:48 +0800

    Add .vistweaks to all the selectors

--- other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
+++ other/gm_scripts/npmjs_visual_tweaks/npmjs_visual_tweaks.user.js
@@ -23,12 +23,12 @@
 
     // Fill (almost) the whole width of the screen, like Wide-Github does.
     //GM_addStyle('.container { width: 98%; max-width: 100%; }');
-    GM_addStyle("#top { max-width: 100%; }");
+    GM_addStyle(".vistweaks #top { max-width: 100%; }");
     // OK not the entire width.  Stop the width when it starts to look ugly.
-    GM_addStyle("#top { max-width: 1500px; }");
+    GM_addStyle(".vistweaks #top { max-width: 1500px; }");
 
     // Set fonts like GitHub
-    GM_addStyle('#readme { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Source Sans Pro", "Lucida Grande", sans-serif; }');
+    GM_addStyle('.vistweaks #readme { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Source Sans Pro", "Lucida Grande", sans-serif; }');
     GM_addStyle('#readme { font-family: line-height: 1.5;}');
     //GM_addStyle('pre, code, kbd, samp { font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace; }');
     // Set fonts how I like them (configurable through browser)
@@ -39,37 +39,37 @@
 
     // Set font sizes like GitHub
     // A snippet of code appearing within a paragraph
-    GM_addStyle('code { font-size: 85%; }');
+    GM_addStyle('.vistweaks code { font-size: 85%; }');
     // A line of code on its own
-    GM_addStyle('pre > code { font-size: 100%; }');
+    GM_addStyle('.vistweaks pre > code { font-size: 100%; }');
     // A block of code
-    GM_addStyle('pre { font-size: 82%; line-height: 1.4; }');
+    GM_addStyle('.vistweaks pre { font-size: 82%; line-height: 1.4; }');
 
     // Darker text
     if (navigator.userAgent.match(/Mac OS X/)) {
         // Weirdly, on Mac, the font strokes on npmjs.com appear slightly thinner than those on Github, for a reason I cannot understand.  To compensate, I use a darker color.
-        GM_addStyle('.markdown p, .markdown li { color: #111; }');
+        GM_addStyle('.vistweaks .markdown p, .vistweaks .markdown li { color: #111; }');
     } else {
         // Github 2016 (my preference)
-        GM_addStyle('.markdown p, .markdown li { color: #333; }');
+        GM_addStyle('.vistweaks .markdown p, .vistweaks .markdown li { color: #333; }');
         // Github 2017
         //GM_addStyle('.markdown p, .markdown li { color: #24292e; }');
     }
 
     // The boxes around inline code snippets
-    GM_addStyle('code { border-radius: 3px; padding: 0.2em 0.4em !important; }');
+    GM_addStyle('.vistweaks code { border-radius: 3px; padding: 0.2em 0.4em !important; }');
 
     // Links should be normal weight (npm makes them bolder)
-    GM_addStyle('.markdown p a, .markdown li a { font-weight: initial; }');
+    GM_addStyle('.vistweaks .markdown p a, .vistweaks .markdown li a { font-weight: initial; }');
 
     // Padding around code blocks and snippets
     // A snippet of code appearing within a paragraph
-    GM_addStyle('code { padding: 0.2em 0.2em; }');
+    GM_addStyle('.vistweaks code { padding: 0.2em 0.2em; }');
     // A line of code on its own
-    GM_addStyle('pre > code { padding: 1em 2em; }');
+    GM_addStyle('.vistweaks pre > code { padding: 1em 2em; }');
 
     // Reduce the large padding inside code blocks to be more like Github's
-    GM_addStyle('pre { padding: 1.1rem !important; }');
+    GM_addStyle('.vistweaks pre { padding: 1.1rem !important; }');
 
     // Lighter background on code blocks and snippets
     GM_addStyle('.vistweaks .markdown .highlight, .vistweaks .markdown code, .vistweaks .markdown pre { background-color: #f6f8fa; }');
@@ -78,7 +78,7 @@
     //GM_addStyle('div, .highlight { padding-bottom: 20px; }');
 
     // Thicker font for section headers
-    GM_addStyle('.markdown h1, .markdown h2, .markdown h3, .markdown h4, .markdown h5, .markdown h6 { font-weight: 600; }');
+    GM_addStyle('.vistweaks .markdown h1, .vistweaks .markdown h2, .vistweaks .markdown h3, .vistweaks .markdown h4, .vistweaks .markdown h5, .vistweaks .markdown h6 { font-weight: 600; }');
 
     // Use the npm font for section headers, even though we aren't using it for main text.  (This is a divergence from Github's markdown scheme.)
     //GM_addStyle(".markdown h1, .markdown h2, .markdown h3, .markdown h4, .markdown h5, .markdown h6 { font-family: 'Source Sans Pro', 'Lucida Grande', sans-serif; }");
@@ -88,15 +88,15 @@
     // Better to do it for the whole content, including the tabs across the top
     //GM_addStyle("#top { padding-left: 1em; padding-right: 1em; }");
     // At larger widths we can enjoy greater padding
-    GM_addStyle("#top { padding-left: 2%; padding-right: 2%; }");
+    GM_addStyle(".vistweaks #top { padding-left: 2%; padding-right: 2%; }");
 
     // Make the sidebar look like a hovering card
     // Sadly this now reaches all the way down to the bottom of the README
-    GM_addStyle(".package__rightSidebar___9dMXo { padding: 0.9em 1.8em 0.1em 1.8em !important; margin-top: 2.75rem; }");
+    GM_addStyle(".vistweaks .package__rightSidebar___9dMXo { padding: 0.9em 1.8em 0.1em 1.8em !important; margin-top: 2.75rem; }");
     //GM_addStyle(".package__rightSidebar___9dMXo { box-shadow: 0 0 8px 0 #00000033; }");
-    GM_addStyle(".package__rightSidebar___9dMXo { border: 1px solid #0002 }");
+    GM_addStyle(".vistweaks .package__rightSidebar___9dMXo { border: 1px solid #0002 }");
     // This widens the gap between the two panels, so the shadow doesn't overlap too much
-    GM_addStyle(".markdown { padding-right: 0.5em; }");
+    GM_addStyle(".vistweaks .markdown { padding-right: 0.5em; }");
     // This graph does not scale down well to low resolutions, with or without our changes.  I will wait and see if they fix that.
 
     // This is one way of increasing the specificity of our selectors
@@ -145,11 +145,11 @@
             sidebarElement.classList.remove('w-third-l');
             sidebarContainer.classList.add('w-third-l');
             sidebarContainer.appendChild(sidebarElement);
-            GM_addStyle(".markdown { padding-right: 0; }");
+            GM_addStyle(".vistweaks .markdown { padding-right: 0; }");
             // Clear the existing margin.  Leave a small margin for the shadow.
-            GM_addStyle(".mr3-ns { margin-right: 4px; }");
+            GM_addStyle(".vistweaks .mr3-ns { margin-right: 4px; }");
             // Give the info card equal padding at the top and bottom
-            GM_addStyle(".package__rightSidebar___9dMXo { padding-top: 1em !important; padding-bottom: 1em !important; }");
+            GM_addStyle(".vistweaks .package__rightSidebar___9dMXo { padding-top: 1em !important; padding-bottom: 1em !important; }");
             //readmeElement.appendChild(sidebarElement);
             readmeElement.parentNode.insertBefore(sidebarContainer, readmeElement);
 
