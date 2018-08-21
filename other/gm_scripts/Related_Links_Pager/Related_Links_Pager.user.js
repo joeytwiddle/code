959b5f823fc9670cde93d665f7501beff352989f
Paul Joey Clark <joeytwiddle@gmail.com>
2018-08-22 00:48:12 +0800

    Get RelatedLinksPager working without #siblings in Firefox

--- other/gm_scripts/Related_Links_Pager/Related_Links_Pager.user.js
+++ other/gm_scripts/Related_Links_Pager/Related_Links_Pager.user.js
@@ -2,7 +2,7 @@
 // @name           Related Links Pager
 // @namespace      RLP
 // @description    Navigate sideways!  When you click a link, related links on the current page are carried with you.  They can be accessed from a pager on the target page, so you won't have to go back in your browser.
-// @version        1.3.25
+// @version        1.3.26
 // @license        AGPL-3.0; http://www.gnu.org/licenses/agpl.txt
 // @downstreamURL  http://userscripts.org/scripts/source/124293.user.js
 // @include        http://*/*
@@ -80,10 +80,13 @@ var useLocalStorageWhenPossible = true; // Hide #siblings from URL when we are t
 //// We previous used this check, but unfortunately it is also true for Tampermonkey on Chrome, which is undesirable.
 // var installedAsChromeExtension = !!window.navigator.vendor.match(/Google/);
 //// So better if you just configure it manually.
-var installedAsChromeExtension = false;
+var installedAsChromeExtension = false;   // replace this with auto-detection if you can!
 var forcePolyfillForGM_setValue = installedAsChromeExtension;
 
-var canGM_setValueCrossDomains = typeof GM_setValue === 'function' && !installedAsChromeExtension;
+//// And another duck-typing failure: In Firefox Greasemonkey, typeof GM_setValue is undefined, even if it is present, so we can't auto-detect it.
+var usingFirefoxGreasemonkey = true;   // replace this with auto-detection if you can!
+
+var canGM_setValueCrossDomains = typeof GM_setValue === 'function' && !installedAsChromeExtension || usingFirefoxGreasemonkey;
 
 // New and delicious feature!  Now works on those pesky singla-page-apps that the kids keep writing.
 var detectPushStateNavigation = true;
