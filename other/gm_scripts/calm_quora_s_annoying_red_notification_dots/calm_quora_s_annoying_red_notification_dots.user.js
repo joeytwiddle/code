b001eed9d8fb48c6b9781d2b91311bcf3a3718b0
Paul Joey Clark <joeytwiddle@gmail.com>
2018-12-23 18:30:38 +0800

    Start Quora scripts on DOMContentLoaded (for Firefox?)

--- other/gm_scripts/calm_quora_s_annoying_red_notification_dots/calm_quora_s_annoying_red_notification_dots.user.js
+++ other/gm_scripts/calm_quora_s_annoying_red_notification_dots/calm_quora_s_annoying_red_notification_dots.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Calm Quora's annoying red notification dots
 // @namespace    joeytwiddle
-// @version      1.0.11
+// @version      1.0.12
 // @license      MIT
 // @description  The red notifications on Quora are too glaring, appear too frequently, and do not go away easily enough.  Let's make them grey so they aren't such a bother.  Also the popups and the adverts can take a hike.
 // @author       joeytwiddle
@@ -26,7 +26,8 @@ var increaseSeparationOfAnswers1 = true;
 var increaseSeparationOfAnswers2 = true;
 
 function afterPageLoad (callback) {
-    setTimeout(callback, 400);
+    //setTimeout(callback, 400);
+    document.addEventListener('DOMContentLoaded', callback);
 }
 
 if (makeRedNotificationsGrey) {
