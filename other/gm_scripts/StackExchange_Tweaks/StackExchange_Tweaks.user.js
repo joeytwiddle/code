c3a86efd158f8c20bd74ee50c563518bb475a1a9
Paul Joey Clark <joeytwiddle@gmail.com>
2019-01-28 16:00:17 +0800

    Fix left border for earthscience site

--- other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
+++ other/gm_scripts/StackExchange_Tweaks/StackExchange_Tweaks.user.js
@@ -2,7 +2,7 @@
 // @name           StackExchange Tweaks
 // @namespace      SET
 // @description    Minor visual tweaks to StackExchange (remove the new sidebar from Q/A pages, for classic look)
-// @version        1.0.9
+// @version        1.0.10
 // @license        MIT
 // @include        https://stackoverflow.com/*
 // @include        https://superuser.com/*
@@ -43,7 +43,7 @@ if (swapProfileAndButtons) {
 if (hideSidebarOnQuestionPages) {
     if (document.location.pathname.match(/^\/(q|questions)\//)) {
         GM_addStyle('#left-sidebar { display: none; }');
-        if (document.location.hostname.match(/^(stackoverflow.com|(politics|physics).stackexchange.com)$/)) {
+        if (document.location.hostname.match(/^(stackoverflow.com|(politics|physics|earthscience).stackexchange.com)$/)) {
             GM_addStyle('#content { border-left: none; }');
         }
     }
