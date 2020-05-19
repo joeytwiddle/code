5eb179f7b278ce51c373a75bb5ceed14742b693a
Paul Joey Clark <joeytwiddle@gmail.com>
2020-05-19 19:20:33 +0800

    Add new userscript Tampermonkey_Google_Drive_sync_fix

---
+++ other/gm_scripts/Tampermonkey_Google_Drive_sync_fix/Tampermonkey_Google_Drive_sync_fix.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Tampermonkey Google Drive sync fix
+// @namespace    https://github.com/Tampermonkey/tampermonkey/issues/904
+// @version      0.1.1
+// @description  Tampermonkey keeps asking us to select an account when we open the browser.  But this script by Jack'lul will do it for us!
+// @author       jacklul <jacklul.github.io>
+// @license      ISC
+// @match        https://accounts.google.com/signin/oauth*tampermonkey*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let tryClick = setInterval(function() {
+        const el = document.querySelector('div[data-authuser="0"] #profileIdentifier');
+
+        if (el) {
+            const isBusy = document.querySelector('div[role="presentation"][tabindex="-1"]');
+            const hasBackButton = document.querySelector('div[role="button"]#next');
+
+            if (isBusy) {
+                console.log('Page is busy...');
+            }
+
+            // Close page if there was "unknown error"
+            if (hasBackButton) {
+                window.close();
+            }
+
+            // Try clicking until page gets busy
+            if (!isBusy) {
+                console.log('Clicking...');
+                el.click();
+            }
+        }
+    }, 500);
+})();
