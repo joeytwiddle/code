ef6d259d57a109b910a7c60dbb9e88eaf2daa0e3
Paul Joey Clark <joeytwiddle@gmail.com>
2022-11-19 15:06:59 +0800

    Bump version

--- other/gm_scripts/Add_movie_ratings_to_IMDB_links/Add_movie_ratings_to_IMDB_links.user.js
+++ other/gm_scripts/Add_movie_ratings_to_IMDB_links/Add_movie_ratings_to_IMDB_links.user.js
@@ -2,7 +2,7 @@
 // @name         Add movie ratings to IMDB links [adopted]
 // @description  Adds movie ratings and number of voters to links on IMDB. Modified version of http://userscripts.org/scripts/show/96884
 // @author       StackOverflow community (especially Brock Adams)
-// @version      2015-11-24-32-joeytwiddle
+// @version      2015-11-24-33-joeytwiddle
 // @license      MIT
 // @match        *://www.imdb.com/*
 // @grant        GM_xmlhttpRequest
