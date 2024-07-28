9fca74b542ce6e26ad9d612185489d1bf77a4cf4
Paul Joey Clark <joeytwiddle@gmail.com>
2024-07-28 12:10:20 +0100

    Rerun watcher on YouTube

--- other/gm_scripts/joeys_global_tweaks/joeys_global_tweaks.user.js
+++ other/gm_scripts/joeys_global_tweaks/joeys_global_tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Joey's Global Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      2024-07-28
+// @version      2024-07-28-2
 // @description  CSS tweaks for various extensions or common elements
 // @author       joeytwiddle
 // @match        http://*/*
@@ -13,7 +13,13 @@
 (function() {
     'use strict';
 
-	runWhenReady('#readergpt__shadowRoot', () => {
+	startWatchingForChatGPTSummary();
+
+	function startWatchingForChatGPTSummary() {
+		runWhenReady('#readergpt__shadowRoot', addStylesToChatGPTSummary);
+	}
+
+	function addStylesToChatGPTSummary() {
 		addStyleToShadowDOM(document.querySelector('#readergpt__shadowRoot'), `
 			/* For "ChatGPT Summary - summarize assistant" extension
 			   https://chromewebstore.google.com/detail/chatgpt-summary-summarize/mikcekmbahpbehdpakenaknkkedeonhf
@@ -21,9 +27,24 @@
 			.readergpt__container {
 				width: 75vw !important;
 				height: 75vh !important;
+				/* To prevent the unneccessary horizontal scrollbar */
+				overflow-x: hidden;
+			}
+			#readergpt__readableText {
+				/* On my machine, the default margin of 16px cuts off the last line of text, let alone showing the white border. */
+				margin-bottom: 56px;
 			}
 		`);
-	});
+
+		if (document.location.hostname.match(/youtube/)) {
+			// It is common on YouTube that I will navigate to another video and run the summarize assistant there too.
+			// In which case, we will need to add the CSS styles again.
+			//
+			// BUG TODO: Of course this doesn't work, because it sees the selector immediately, and triggers a second time!
+			//
+			//startWatchingForChatGPTSummary();
+		}
+	}
 
 	function addStyleToShadowDOM(shadowWrapper, css) {
 		GM_addElement(shadowWrapper.shadowRoot, 'style', { textContent: css });
